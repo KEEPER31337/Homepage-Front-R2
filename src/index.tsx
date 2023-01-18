09a93aa6b5a109cd2b7889b478a102f336fc9624
lcqff <gooyeonhu82@gmail.com>
2023-01-19 06:19:02 +0900

    chore : tailwind 적용
    - tailwind 적용...
    
    #103

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      basic: ['GowunDodum-Regular'],
+      title: ['CBNUJIKJI'],
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      colors: {
+        mainHeader: 'rgb(255,255,255,0)',
+        mainBlack: '#000000',
+        mainWhite: '#ffffff',
+        mainYellow: '#fbbf24',
+        pointYellow: '#f59e0b',
+        backGray: '#efefef',
+        divisionGray: '#e0e0e0',
+        darkComponent: '#1f2937',
+        darkPoint: '#111827',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+};
