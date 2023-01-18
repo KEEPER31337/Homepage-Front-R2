a35df0f4031f657a5bcddaf620a847f17605bf2a
lcqff <gooyeonhu82@gmail.com>
2023-01-19 06:20:55 +0900

    feat : 헤더 제작
    - 임시로 이전 로고 가져옴
    - 색상 변경
    - 아이콘 클릭시 링크 이동
    
    #103

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import useMainRouter from './router/useMainRouter';
+import Header from './pages/shared/Header';
 
 const App = () => {
   const routes = useMainRouter();
 
-  return <div className="App">{routes}</div>;
+  return (
+    <>
+      <Header />
+      <div className="App">{routes}</div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/shared/Header.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+
+// local
+import Logo from '../../assets/keeper_logo.png';
+import SignInBox from './Component/SignInBox';
+import HeaderIcon from './Component/HeaderIcon';
+
+const Header: React.FC = () => {
+  // const jobs = member?.memberInfo?.jobs;
+
+  return (
+    <Popover className="flex h-[66px] bg-[#131316] border-t-0 border-r-0 border-b border-l-0 border-[#4CEEF9]">
+      <div className="flex w-full items-center justify-between">
+        <Link to="/">
+          <img className="ml-[33px] w-[105px] h-[41px] object-cover" src={Logo} alt="" />
+        </Link>
+        <div className="mr-[19px]">
+          <HeaderIcon />
+        </div>
+      </div>
+    </Popover>
+  );
+};
+
+export default Header;
