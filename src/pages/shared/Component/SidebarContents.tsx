67c492494d3bd024b789a4491b4d2f52af831803
lcqff <71930280+lcqff@users.noreply.github.com>
2023-01-20 18:57:02 +0900

    feat : 카테고리 css 변화
    - category.js 상위 카테고리 하위 카테고리 구분을 위한 id값 수정
    - 카테고리 클릭시 백그라운드 컬러 유지, 폰트 스타일 유지
    
    #104

--- src/pages/shared/Component/SidebarContents.tsx
+++ src/pages/shared/Component/SidebarContents.tsx
@@ -1,5 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Popover, Transition } from '@headlessui/react';
 import { Link } from 'react-router-dom';
 import { VscCircleFilled } from 'react-icons/vsc';
@@ -11,26 +11,51 @@ interface SidebarContentsProps {
 }
 
 const SidebarContents = ({ category }: SidebarContentsProps) => {
+  const [selectedCategory, setSelectedCategory] = useState<number>(0);
+  const [categoryTint, setCategoryTint] = useState<string>('');
   useEffect(() => {
-    if (category) {
-      console.log(category);
-      console.log(category.name);
-    }
+    setCategoryTint('flex items-center font-light text-white pl-[50px] w-full h-[45px] bg-[#131316]');
   }, [category]);
+
+  const categoryClicked = (id: number) => {
+    setSelectedCategory(id);
+  };
+
+  useEffect(() => {
+    console.log(selectedCategory);
+    if (selectedCategory % 10 === category.id) {
+      setCategoryTint('flex items-center text-white pl-[50px] w-full h-[45px] bg-[#4ceef9]/30 font-medium ');
+    } else setCategoryTint('flex items-center font-light text-white pl-[50px] w-full h-[45px] bg-[#131316]');
+  }, [selectedCategory]);
 
   return (
     <Popover>
-      <Popover.Button className="flex items-center text-white pl-[50px] w-full h-[45px] hover:bg-[#4ceef9]/30 ">
+      <Popover.Button className={categoryTint}>
         <div>{category?.name}</div>
       </Popover.Button>
       <Popover.Panel>
-        <div className="flex flex-col relative bg-[#131316] text-white">
-          {category.subs.map((item: any) => (
-            <Link to="#!" className="flex items-center pl-[50px] h-[40px] ">
-              <VscCircleFilled className="inline-block" />
-              <div className="inline-block">&nbsp;{item.name}</div>
-            </Link>
-          ))}
+        <div className="flex flex-col relative bg-[#131316] font-light text-white">
+          {category.subs.map((item: any) => {
+            if (item.id >= 1000) {
+              return (
+                <div className="flex items-center pl-[50px] mt-[10px] h-[40px] text-sm font-light">{item.name}</div>
+              );
+            }
+            if (item.id === selectedCategory) {
+              return (
+                <Link to="#!" className="flex items-center pl-[50px] h-[40px] ">
+                  <VscCircleFilled className="inline-block" />
+                  <div className="inline-block">&nbsp;{item.name}</div>
+                </Link>
+              );
+            }
+            return (
+              <Link to="#!" className="flex items-center pl-[50px] h-[40px]" onClick={() => categoryClicked(item.id)}>
+                <VscCircleFilled className="inline-block" />
+                <div className="inline-block">&nbsp;{item.name}</div>
+              </Link>
+            );
+          })}
         </div>
       </Popover.Panel>
     </Popover>
