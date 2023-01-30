d0c445091cecff520feac0ef8c330988af587873
jasper200207 <jasper12394@gmail.com>
2023-01-30 23:56:44 +0900

    feat : 메인 로고
    #116

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import MainLogo from '../../assets/img/keeper_main_logo.png';
 
 const About = () => {
   return (
     <div className="flex bg-galaxy bg-contain bg-repeat">
-      <div className="flex h-[100vh] w-full" />
+      <div className="flex h-[100vh] w-full">
+        <div className="m-auto">
+          <img src={MainLogo} width={800} alt="keeper_main_logo" />
+        </div>
+      </div>
     </div>
   );
 };
