7ac23abd7c485b7db9d55e513644cbba4cbe003d
lcqff <71930280+lcqff@users.noreply.github.com>
2023-01-30 15:53:08 +0900

    refactor : withMT빠진 거 추가
    #104

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require('@material-tailwind/react/utils/withMT');
 
-module.exports = {
+module.exports = withMT{
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
