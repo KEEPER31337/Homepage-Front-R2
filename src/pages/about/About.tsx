928c347d55ec5929a7510a6694fc255627f02765
jasper200207 <jasper12394@gmail.com>
2023-01-30 23:57:41 +0900

    feat : 메인 소개 문구
    #116

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -7,6 +7,10 @@ const About = () => {
       <div className="flex h-[100vh] w-full">
         <div className="m-auto">
           <img src={MainLogo} width={800} alt="keeper_main_logo" />
+          <p className="mx-auto text-center text-white">‘지키다’라는 의미를 가진 단어 ‘keep’에서 착안하여,</p>
+          <p className="mx-auto text-center text-white">
+            정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다.
+          </p>
         </div>
       </div>
     </div>
