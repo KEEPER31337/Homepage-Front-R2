a708bd5584ac50272603b1a7053e55030721d29f
mia2583 <mia2583@pusan.ac.kr>
2023-01-31 11:19:49 +0900

    refactor : 이전 코드로 복구 - 페이지를 직접 접근이 아닌 라우터 사용
    #131

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import useMainRouter from '@router/useMainRouter';
-import SeminarManage from '@pages/admin/SeminarManage';
 
 const App = () => {
   const routes = useMainRouter();
 
-  return (
-    <div className="App">
-      {routes}
-      <SeminarManage />
-    </div>
-  );
+  return <div className="App">{routes}</div>;
 };
 
 export default App;
