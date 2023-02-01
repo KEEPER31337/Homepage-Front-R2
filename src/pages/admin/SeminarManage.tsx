fa53ce006fadd7548929cfde86a057f5d36197f3
mia2583 <mia2583@pusan.ac.kr>
2023-02-01 10:53:54 +0900

    refactor : 테스트 코드 삭제 - 불필요한 테스트코드 삭제
    #131

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import ActionModal from '@components/modal/ActionModal';
 
 const SeminarManage = () => {
-  return (
-    <div>
-      세미나 관리
-      <div>
-        <div>모달 테스트</div>
-        <ActionModal title="제목" contents="내용" code="<p>코드동작확인</p>" buttonName="확인" onClick={action} />
-      </div>
-    </div>
-  );
+  return <div>세미나 관리</div>;
 };
 
 export default SeminarManage;
