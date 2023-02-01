d2f90a878929f2cc3de4f1863c4f415eef6e9192
mia2583 <mia2583@pusan.ac.kr>
2023-02-01 10:49:52 +0900

    style : tailwindcss plugin 적용 - css 적용 순서 변경
    #131

--- src/components/modal/ActionModal.tsx
+++ src/components/modal/ActionModal.tsx
@@ -17,8 +17,8 @@ const ActionModal = ({ title, contents, code, buttonName, onClick }: ModalProps)
   return (
     <Dialog open={open} handler={handleOpen} className="rounded-lg bg-[#26262c]">
       <div className="mt-7 ml-10 mb-2 mr-2">
-        <DialogHeader className="font-bold text-left text-[#4ceef9]">{title}</DialogHeader>
-        <DialogBody className="text-base text-left text-white">
+        <DialogHeader className="text-left font-bold text-[#4ceef9]">{title}</DialogHeader>
+        <DialogBody className="text-left text-base text-white">
           {contents}
           <div dangerouslySetInnerHTML={{ __html: code }} />
         </DialogBody>
@@ -26,14 +26,14 @@ const ActionModal = ({ title, contents, code, buttonName, onClick }: ModalProps)
           <Button
             variant="text"
             onClick={handleOpen}
-            className="rounded-sm flex-grow-0 flex-shrink-0 text-xs font-semibold text-center text-[#4ceef9]"
+            className="flex-shrink-0 flex-grow-0 rounded-sm text-center text-xs font-semibold text-[#4ceef9]"
           >
             <span>취소</span>
           </Button>
           <Button
             variant="outlined"
             onClick={onClick}
-            className="rounded-sm flex-grow-0 flex-shrink-0 border-none text-xs font-semibold text-center text-[#26262c] bg-[#4ceef9]"
+            className="flex-shrink-0 flex-grow-0 rounded-sm border-none bg-[#4ceef9] text-center text-xs font-semibold text-[#26262c]"
           >
             <span>{buttonName}</span>
           </Button>
