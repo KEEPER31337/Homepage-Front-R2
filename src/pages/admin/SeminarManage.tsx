d966648e15e5e371a636a5a2b8b570f6fb339cfe
mia2583 <mia2583@pusan.ac.kr>
2023-02-02 23:37:39 +0900

    chore : import 선언하기 - useState 관련 임포트 - 공통 테이블 컴포넌트 임포트
    #140

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import StandardTable from '@components/Table/StandardTable';
+
 const SeminarContent = () => {
   const [listColumns, setListColumns] = useState([
   ]);
