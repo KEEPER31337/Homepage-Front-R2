e90638cefbb6e3676687f40953e9199fde051ddf
mia2583 <mia2583@pusan.ac.kr>
2023-02-02 16:10:53 +0900

    design : 세미나 페이지 전체 틀 - 타이틀, 사이드바, 제작 페이지 위치 분리 - 세연, 서윤 코드 참고
    #120

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+const SeminarContent = () => {
+};
 
 const SeminarManage = () => {
   return <div>세미나 관리</div>;
+  return (
+    <div>
+      <div>Keeper Title</div>
+      <div className="flex h-[100vh] w-[100vw] bg-gray-300">
+        <div className="h-[100vh] w-[320px] border border-black bg-white">사이드 바</div>
+        <div className="flex w-full justify-center">
+          <div className="h-full w-full max-w-[1080px] border border-black bg-white">
+            세미나 페이지
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SeminarManage;
