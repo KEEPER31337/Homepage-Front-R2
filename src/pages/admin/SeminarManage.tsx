4132e022e6564971f94ba4e405006edb13e08a74
mia2583 <mia2583@pusan.ac.kr>
2023-02-02 23:10:29 +0900

    feat : 세미나 추가 - 추가 버튼 클릭 시 새로운 세미나 추가 - 추가된 세미나 "출석 전"으로 채우기
    #139

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -4,6 +4,22 @@ const SeminarContent = () => {
   ]);
   const [listRows, setListRows] = useState([
   ]);
+  const [newDate, setNewDate] = useState();
+  const addSeminar = () => {
+    setListColumns([
+      ...listColumns,
+      {
+        key: `date${newDate}` as keyof SeminarListRow,
+        headerName: `${newDate}`,
+      },
+    ]);
+    setListRows(
+      listRows.map((row) => {
+        return { ...row, [`date${newDate}`]: '출석 전' };
+      }),
+    );
+  };
+
   return (
     <div className="flex h-full flex-col space-y-[32px] bg-subBlack px-20 pt-[80px]">
       <p className="font-[28pt] text-pointBlue">세미나 관리(2023년 1학기)</p>
@@ -11,7 +27,7 @@ const SeminarContent = () => {
         <div className="m-10 grow">
           <StandardTable columns={listColumns} rows={listRows} />
           <div className="flex justify-end">
-            <button type="button" className="m-1 bg-black text-white">
+            <button type="button" className="m-1 bg-black text-white" onClick={addSeminar}>
               추가
             </button>
             <button type="button" className="m-1 bg-black text-white">
