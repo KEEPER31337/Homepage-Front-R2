3d925082e6dbd124212ceaf452ccdf8d5a702d10
mia2583 <mia2583@pusan.ac.kr>
2023-02-02 23:43:04 +0900

    chore : 세미나 관리 페이지 UI 적용되게 하기 - 중복된 return 삭제 - 세미나 관리 페이지 내에 주요 내용 보이게 하기
    #120

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -43,7 +43,6 @@ const SeminarContent = () => {
 };
 
 const SeminarManage = () => {
-  return <div>세미나 관리</div>;
   return (
     <div>
       <div>Keeper Title</div>
@@ -51,7 +50,7 @@ const SeminarManage = () => {
         <div className="h-[100vh] w-[320px] border border-black bg-white">사이드 바</div>
         <div className="flex w-full justify-center">
           <div className="h-full w-full max-w-[1080px] border border-black bg-white">
-            세미나 페이지
+            <SeminarContent />
           </div>
         </div>
       </div>
