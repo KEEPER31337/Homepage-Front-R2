e879a389694b63e0c7d8041d12a2646cc9be6d4e
publdaze <publdaze@naver.com>
2023-02-06 06:29:59 +0900

    feat : 사이드바 최소 규격 지정
    - container 내부 내용 크기에 의해 사이드바 width가 변경됨을 막기 위해 최소 규격 지정
    
    #102

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Sidebar = () => {
-  return <div className="flex h-screen w-sidebar bg-mainBlack pt-header" />;
+  return <div className="flex h-screen w-sidebar min-w-sidebar bg-mainBlack pt-header" />;
 };
 
 export default Sidebar;
