da06a087a75ac4464c948d677d5202a5c3968682
publdaze <publdaze@naver.com>
2023-02-06 05:48:41 +0900

    feat : 레이아웃 뼈대 구성
    - 사이드바, 헤더 구성
    
    #102

--- src/router/useMainRouter.tsx
+++ src/router/useMainRouter.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import Home from '@pages/home/Home';
 import SeminarManage from '@pages/admin/SeminarManage';
+import MainLayout from '@components/Layout/MainLayout';
 
 const useMainRouter = () =>
   useRoutes([
     {
       path: '/',
+      element: <MainLayout />,
       children: [
         {
           index: true,
