7445eff83ffd1dcad702622ae8176ed8b11c34f6
publdaze <publdaze@naver.com>
2023-02-05 15:43:00 +0900

    feat : react-query 사용을 위한 초기 세팅
    - 최상단에 Context 래핑
    
    #177

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-tailwind/react';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import './tailwind.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider>
         <BrowserRouter>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </BrowserRouter>
       </ThemeProvider>
     </RecoilRoot>
