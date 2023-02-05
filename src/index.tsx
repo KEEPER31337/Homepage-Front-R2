1aec9f09e32b08c7709d26052e11e1bc63fa98b8
publdaze <publdaze@naver.com>
2023-02-05 17:39:23 +0900

    feat : 윈도우 포커스 될 때마다 refetch 되는 기능 끄기
    - 매번 포커스가 돌아올 때 갱신되어야할 필요성보다는 api 호출이 과도하게 되는 경우를 컨트롤 하는 것이 더 중요하다 판단하여 기본 옵션으로 refetchOnWindowFocus 기능 꺼둠
    
    #177

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,13 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 root.render(
   <React.StrictMode>
