e46107ba5533171c5f5fbc46c659d96b05944681
publdaze <publdaze@naver.com>
2023-02-06 18:55:30 +0900

    feat : about api 상태 관리 부분 작성
    #177

--- src/api/aboutApi.ts
+++ src/api/aboutApi.ts
@@ -1,7 +1,17 @@
-const getUserInfo = selector({
-  key: 'getUserInfo',
-  get: async ({ get }) => {
-    const response = await axios.get('https://some-url.com');
-    return response.data;
-  },
-});
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { PageBlockInfo } from './dto';
+
+const useGetTitleTypesQuery = () => {
+  const fetcher = () => axios.get(`/about/titles/types`).then(({ data }) => data.list);
+
+  return useQuery<string[]>('titleType', fetcher);
+};
+
+const useGetBlockListQuery = ({ type }: { type: string }) => {
+  const fetcher = () => axios.get(`/about/titles/types/${type}`).then(({ data }) => data);
+
+  return useQuery<PageBlockInfo>('blockList', fetcher);
+};
+
+export { useGetTitleTypesQuery, useGetBlockListQuery };
