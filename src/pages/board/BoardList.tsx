5debc85e22430b2bd102ac4c0990fa062cd087aa
koty08 <koty08@naver.com>
2023-02-12 13:25:31 +0900

    refactor: 더미 데이터 위치 변경
    - 전역 변수로 변경
    
    #132

--- src/pages/board/BoardList.tsx
+++ src/pages/board/BoardList.tsx
@@ -20,27 +20,28 @@ interface BoardColumn {
   headerName: string;
 }
 
+const dummyColumn: BoardColumn[] = [
+  { key: 'no', headerName: '번호' },
+  { key: 'title', headerName: '제목' },
+  { key: 'writer', headerName: '작성자' },
+  { key: 'registerDate', headerName: '작성일' },
+  { key: 'watched', headerName: '조회수' },
+];
+
+const dummyRow: BoardRow[] = [
+  { id: 1, no: 1, title: '아무제목이나적어1', writer: '랄랄라', registerDate: '2023.02.09', watched: 12 },
+  { id: 2, no: 2, title: '아무제목이나적어2', writer: '아무개', registerDate: '2023.02.08', watched: 34 },
+  { id: 3, no: 3, title: '아무제목이나적어3', writer: '김개똥', registerDate: '2023.02.07', watched: 56 },
+  { id: 4, no: 4, title: '아무제목이나적어4', writer: '랄랄라', registerDate: '2023.02.09', watched: 12 },
+  { id: 5, no: 5, title: '아무제목이나적어5', writer: '아무개', registerDate: '2023.02.08', watched: 34 },
+  { id: 6, no: 6, title: '아무제목이나적어6', writer: '김개똥', registerDate: '2023.02.07', watched: 56 },
+  { id: 7, no: 7, title: '아무제목이나적어7', writer: '랄랄라', registerDate: '2023.02.09', watched: 12 },
+  { id: 8, no: 8, title: '아무제목이나적어8', writer: '아무개', registerDate: '2023.02.08', watched: 34 },
+];
+
 const BoardList = () => {
   const [searchParams] = useSearchParams();
   const category: string | null = searchParams.get('category');
-  const dummyColumn: BoardColumn[] = [
-    { key: 'no', headerName: '번호' },
-    { key: 'title', headerName: '제목' },
-    { key: 'writer', headerName: '작성자' },
-    { key: 'registerDate', headerName: '작성일' },
-    { key: 'watched', headerName: '조회수' },
-  ];
-
-  const dummyRow: BoardRow[] = [
-    { id: 1, no: 1, title: '아무제목이나적어1', writer: '랄랄라', registerDate: '2023.02.09', watched: 12 },
-    { id: 2, no: 2, title: '아무제목이나적어2', writer: '아무개', registerDate: '2023.02.08', watched: 34 },
-    { id: 3, no: 3, title: '아무제목이나적어3', writer: '김개똥', registerDate: '2023.02.07', watched: 56 },
-    { id: 4, no: 4, title: '아무제목이나적어4', writer: '랄랄라', registerDate: '2023.02.09', watched: 12 },
-    { id: 5, no: 5, title: '아무제목이나적어5', writer: '아무개', registerDate: '2023.02.08', watched: 34 },
-    { id: 6, no: 6, title: '아무제목이나적어6', writer: '김개똥', registerDate: '2023.02.07', watched: 56 },
-    { id: 7, no: 7, title: '아무제목이나적어7', writer: '랄랄라', registerDate: '2023.02.09', watched: 12 },
-    { id: 8, no: 8, title: '아무제목이나적어8', writer: '아무개', registerDate: '2023.02.08', watched: 34 },
-  ];
 
   return (
     <div className="flex h-screen flex-col items-center bg-subBlack pt-24">
