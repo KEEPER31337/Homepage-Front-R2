b552afd1439370df65de46888955378950540736
publdaze <publdaze@naver.com>
2023-02-15 16:23:05 +0900

    design : 페이지네이션 색 적용을 위한 primary 색 커스텀
    #130

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-tailwind/react';
+import { ThemeProvider as MUIThemeProvider } from '@mui/material';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 
 import './tailwind.css';
 import theme from '@constants/materialTailwindTheme';
+import muiTheme from '@constants/muiTheme';
 import App from './App';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
@@ -26,11 +28,13 @@ root.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider value={theme}>
-        <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
-        </BrowserRouter>
+        <MUIThemeProvider theme={muiTheme}>
+          <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
+          </BrowserRouter>
+        </MUIThemeProvider>
       </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>,
