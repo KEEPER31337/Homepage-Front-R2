2272253bb628d49bb1ef478c72ad76d5ce80ac80
publdaze <publdaze@naver.com>
2023-02-15 16:33:56 +0900

    feat : row 정보 라벨 추가
    #130

--- src/components/Pagination/TablePagination.tsx
+++ src/components/Pagination/TablePagination.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Pagination, TablePagination } from '@mui/material';
 
+import './pagination.css';
+
 interface StandardTablePaginationProps {
   rowsPerPage?: number;
 }
@@ -17,6 +19,17 @@ const StandardTablePagination = ({ rowsPerPage = 10 }: StandardTablePaginationPr
 
   return (
     <div className="flex !w-full items-center justify-between bg-middleBlack">
+      <TablePagination
+        className="!m-0 !-ml-2 !border-none !p-0 !text-subGray"
+        count={totalItems}
+        page={page}
+        onPageChange={() => {
+          /* NOTE 필수값이나 커스텀으로 인해 사용하지 않는 핸들러 */
+        }}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[]}
+        labelDisplayedRows={({ from, to, count }) => `Showing ${from}-${to} of ${count} items`}
+      />
       <Pagination
         color="primary"
         className="mr-1 !text-pointBlue"
