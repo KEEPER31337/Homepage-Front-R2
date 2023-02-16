43673b6d0eb7628c5878d3fd288ac9e604d667e1
lcqff <gooyeonhu82@gmail.com>
2023-02-16 17:40:41 +0900

    design: 세미나 관리 버튼, 타이틀 추가
    #172

--- src/pages/senimarAttend/SenimarAttend.tsx
+++ src/pages/senimarAttend/SenimarAttend.tsx
@@ -64,41 +64,47 @@ const SeminarAttend = () => {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <ThemeProvider value={inputTheme}>
-        <Card className="h-[426px] w-96 bg-mainBlack">
-          <CardBody className="my-auto h-[281px] content-between items-center p-0 text-pointBlue">
-            <Typography className="text-center text-h3 font-bold">출석체크</Typography>
-            <Typography className="text-center text-paragraph text-white">{seminarDate} 세미나</Typography>
-            <div className="mx-auto my-[22px] flex h-[52px] w-[192px] justify-between">
-              <Input disabled />
-              <Input disabled />
-              <Input disabled />
-              <Input disabled />
-            </div>
-            <Button className="font-small mx-auto mb-[19px] block flex h-[34px] w-[71px] items-center whitespace-nowrap bg-pointBlue text-mainBlack">
-              시작
-            </Button>
-            <div className=" mx-auto flex w-[125px] items-end whitespace-nowrap">
-              <p className="mr-3">출석</p>
-              <Select variant="standard" value={attendValue} onChange={() => handleAttendChange}>
-                <Option value="5분">5분</Option>
-                <Option value="10분">10분</Option>
-                <Option value="15분">15분</Option>
-              </Select>
-            </div>
-            <div className="mx-auto flex w-[125px] items-end whitespace-nowrap">
-              <p className="mr-3">지각</p>
-              <Select variant="standard" value={lateAttendValue} onChange={() => handleLateChange}>
-                <Option value="5분">5분</Option>
-                <Option value="10분">10분</Option>
-                <Option value="15분">15분</Option>
-              </Select>
-            </div>
-          </CardBody>
-        </Card>
-      </ThemeProvider>
-    </div>
+    <>
+      <PageTitle>세미나 출석체크</PageTitle>
+      <div className="grid justify-items-end">
+        <FilledButton>세미나 관리</FilledButton>
+      </div>
+      <div className="flex h-screen items-center justify-center">
+        <ThemeProvider value={inputTheme}>
+          <Card className="h-[426px] w-96 bg-mainBlack">
+            <CardBody className="my-auto h-[281px] content-between items-center p-0 text-pointBlue">
+              <Typography className="text-center text-h3 font-bold">출석체크</Typography>
+              <Typography className="text-center text-paragraph text-white">{seminarDate} 세미나</Typography>
+              <div className="mx-auto my-[22px] flex h-[52px] w-[192px] justify-between">
+                <Input disabled />
+                <Input disabled />
+                <Input disabled />
+                <Input disabled />
+              </div>
+              <Button className="font-small mx-auto mb-[19px] block flex h-[34px] w-[71px] items-center whitespace-nowrap bg-pointBlue text-mainBlack">
+                시작
+              </Button>
+              <div className=" mx-auto flex w-[125px] items-end whitespace-nowrap">
+                <p className="mr-3">출석</p>
+                <Select variant="standard" value={attendValue} onChange={() => handleAttendChange}>
+                  <Option value="5분">5분</Option>
+                  <Option value="10분">10분</Option>
+                  <Option value="15분">15분</Option>
+                </Select>
+              </div>
+              <div className="mx-auto flex w-[125px] items-end whitespace-nowrap">
+                <p className="mr-3">지각</p>
+                <Select variant="standard" value={lateAttendValue} onChange={() => handleLateChange}>
+                  <Option value="5분">5분</Option>
+                  <Option value="10분">10분</Option>
+                  <Option value="15분">15분</Option>
+                </Select>
+              </div>
+            </CardBody>
+          </Card>
+        </ThemeProvider>
+      </div>
+    </>
   );
 };
 
