d55707aefe0b6e02ff9b1ceb9470fda5502efb5e
mia2583 <mia2583@pusan.ac.kr>
2023-02-18 14:28:10 +0900

    feat : 공통 컨폼 모달 - 기본적인 UI - x 아이콘으로 창 닫기
    #227

---
+++ src/components/Modal/ConfirmModal.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import { Button, Dialog, DialogHeader, DialogBody, DialogFooter } from '@material-tailwind/react';
+import FilledButton from '@components/Button/FilledButton';
+import { VscChromeClose } from 'react-icons/vsc/';
+
+interface ConfirmModalProps {
+  opened: boolean;
+  handleOpen: () => void;
+  title: string;
+  children: React.ReactNode;
+}
+
+const ConfirmModal = ({ opened, handleOpen, title, children }: ConfirmModalProps) => {
+  return (
+    <Dialog open={opened} handler={handleOpen} className="rounded-lg bg-[#26262c]">
+      <div className="mt-5 ml-10 mb-2 mr-2">
+        <div className="mr-5 flex items-center justify-end">
+          <VscChromeClose onClick={handleOpen} />
+        </div>
+        <DialogHeader className="text-left font-bold text-[#4ceef9]">{title}</DialogHeader>
+        <DialogBody className="mb-6 text-left text-base text-white">{children}</DialogBody>
+      </div>
+    </Dialog>
+  );
+};
+
+export default ConfirmModal;
