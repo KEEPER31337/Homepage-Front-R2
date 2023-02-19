396faf2cd345f9155e95028802a618f7a08f9037
publdaze <publdaze@naver.com>
2023-02-19 12:16:52 +0900

    refactor : 임시 레이아웃 제거
    #120

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -14,20 +14,16 @@ const SeminarManage = () => {
   };
 
   return (
-    <div className="flex h-full flex-col space-y-[32px] bg-subBlack px-20 pt-[80px]">
+    <div>
       <p className="font-[28pt] text-pointBlue">세미나 관리(2023년 1학기)</p>
-      <div className="flex h-fit w-full justify-between">
-        <div className="m-10 grow">
-          <StandardTable columns={listColumns} rows={listRows} />
-          <div className="mt-4 flex justify-end space-x-2">
-            <Button variant="outlined" className="border-pointBlue text-pointBlue" onClick={addSeminar}>
-              추가
-            </Button>
-            <Button variant="outlined" className="border-2 border-pointBlue text-pointBlue">
-              삭제
-            </Button>
-          </div>
-        </div>
+      <StandardTable columns={listColumns} rows={listRows} />
+      <div className="mt-4 flex justify-end space-x-2">
+        <Button variant="outlined" className="border-pointBlue text-pointBlue" onClick={addSeminar}>
+          추가
+        </Button>
+        <Button variant="outlined" className="border-2 border-pointBlue text-pointBlue">
+          삭제
+        </Button>
       </div>
     </div>
   );
