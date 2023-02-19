6caa944cf0671d1c0be3a62d70512ee77aaf4d80
publdaze <publdaze@naver.com>
2023-02-19 14:49:49 +0900

    refactor : 미사용 코드 제거
    #120

--- src/pages/admin/SeminarManage.tsx
+++ src/pages/admin/SeminarManage.tsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import StandardTable from '@components/Table/StandardTable';
-import { Button } from '@mui/material';
 import { columns, rows } from '@mocks/SeminarManageApi';
-import type { SeminarListRow } from '@mocks/SeminarManageApi';
 import PageTitle from '@components/Typography/PageTitle';
 import OutlinedButton from '@components/Button/OutlinedButton';
 
 const SeminarManage = () => {
-  const [listColumns, setListColumns] = useState(columns);
-  const [listRows, setListRows] = useState(rows);
-  const [newDate, setNewDate] = useState();
+  const listColumns = columns;
+  const listRows = rows;
 
   const addSeminar = () => {
     // TODO
