29e8fc596005a8638be93b9f5060e2c72f029f2d
jasper200207 <jasper12394@gmail.com>
2023-02-22 18:41:11 +0900

    feat : 이미지 배경 적용

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-  return <div />;
+  return (
+    <div className="flex w-[calc(100vw-theme(space.sidebar))] bg-galaxy bg-contain bg-repeat">
+      <div className="flex h-screen w-full" />
+    </div>
+  );
 };
 
 export default Home;
