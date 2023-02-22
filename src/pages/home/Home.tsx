f56d0d657da6b16076cb4f9bdfb9f43209c7defd
jasper200207 <jasper12394@gmail.com>
2023-02-22 18:45:01 +0900

    feat : 메인로고

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ReactComponent as LogoNeon } from '@assets/logo/logo_neon.svg';
 
 const Home = () => {
   return (
     <div className="flex w-[calc(100vw-theme(space.sidebar))] bg-galaxy bg-contain bg-repeat">
-      <div className="flex h-screen w-full" />
+      <div className="flex h-screen w-full">
+        <LogoNeon className="m-auto w-[700px]" />
+      </div>
     </div>
   );
 };
