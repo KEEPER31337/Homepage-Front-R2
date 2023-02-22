5754f136a86f1a941dc1797145141d9fd930f532
jasper200207 <jasper12394@gmail.com>
2023-02-22 18:54:35 +0900

    feat : 메인 소개 문구

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { ReactComponent as LogoNeon } from '@assets/logo/logo_neon.svg';
 
+const introText =
+  "'지키다'라는 의미를 가진 단어 'keep'에서 착안하여,\n정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다.";
+
 const Home = () => {
   return (
     <div className="flex w-[calc(100vw-theme(space.sidebar))] bg-galaxy bg-contain bg-repeat">
-      <div className="flex h-screen w-full">
-        <LogoNeon className="m-auto w-[700px]" />
+      {/* Intro */}
+      <div className="flex h-[calc(100vh-theme(space.header))] w-full flex-col">
+        <div className="m-auto">
+          <LogoNeon className="my-20 w-[700px]" />
+          <p className="mx-auto whitespace-pre-wrap text-center text-white">{introText}</p>
+        </div>
       </div>
     </div>
   );
