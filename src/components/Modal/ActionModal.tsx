299247b851a04fc0eda4787b1f84eaaadd57bb84
mia2583 <mia2583@pusan.ac.kr>
2023-02-23 16:36:35 +0900

    design : 커스텀 글자 크기 적용 - typography를 사용하여 커스텀된 글자 크기 적용
    #263

--- src/components/Modal/ActionModal.tsx
+++ src/components/Modal/ActionModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dialog, DialogHeader, DialogBody, DialogFooter } from '@material-tailwind/react';
+import { Dialog, DialogHeader, DialogBody, DialogFooter, Typography } from '@material-tailwind/react';
 import FilledButton from '@components/Button/FilledButton';
 import TextButton from '@components/Button/TextButton';
 
@@ -16,8 +16,12 @@ const ActionModal = ({ opened, handleOpen, title, children, buttonName, onAction
   return (
     <Dialog open={opened} handler={handleOpen} className="rounded-lg bg-subBlack">
       <div className="mt-7 ml-10 mb-2 mr-2">
-        <DialogHeader className="text-left font-bold text-pointBlue">{title}</DialogHeader>
-        <DialogBody className="text-left text-base text-white">{children}</DialogBody>
+        <DialogHeader className="text-left font-bold text-pointBlue">
+          <Typography variant="h3">{title}</Typography>
+        </DialogHeader>
+        <DialogBody className="text-left text-base text-white">
+          <Typography variant="paragraph">{children}</Typography>
+        </DialogBody>
         <DialogFooter className="flex-end items-center gap-5">
           <TextButton onClick={handleOpen}>취소</TextButton>
           <FilledButton onClick={onActionButonClick}>{buttonName}</FilledButton>
