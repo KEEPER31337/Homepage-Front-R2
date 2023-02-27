321d95adb78b8a310613c128685acf3b8962a598
publdaze <publdaze@naver.com>
2023-02-27 16:34:00 +0900

    feat : 그리드뷰 썸네일 관련 기능 추가
    - 그리드뷰 썸네일 데이터 받아오도록 처리
    - 썸네일 없을 시 로고로 띄워주도록 처리
    
    #130

--- src/components/Card/PostingCard.tsx
+++ src/components/Card/PostingCard.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Card, CardContent, CardMedia } from '@mui/material';
+import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
 
-const PostingCard = () => {
+interface PostingCardProps {
+  thumbnailPath: string | null;
+}
+
+const PostingCard = ({ thumbnailPath }: PostingCardProps) => {
   return (
     <Card className="w-52 !rounded-none !bg-middleBlack !bg-none">
-      <CardMedia className="h-[118px] bg-middleBlack" component="img" alt="썸네일" />
+      {thumbnailPath ? (
+        <CardMedia className="h-[118px] bg-middleBlack" component="img" src={thumbnailPath} alt="썸네일" />
+      ) : (
+        <Logo className="m-auto h-[118px] w-28" />
+      )}
       <CardContent className="h-24 !bg-mainBlack" />
     </Card>
   );
