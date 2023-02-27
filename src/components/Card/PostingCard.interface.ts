6d3e50216c0b84c4f1cfd12665734badf20b5fc1
publdaze <publdaze@naver.com>
2023-02-27 16:38:41 +0900

    feat : 그리드뷰 카드 주요 정보 관련 기능 추가
    - 제목, 타입(공지, 비밀글 등) 데이터 받아오기
    
    #130

---
+++ src/components/Card/PostingCard.interface.ts
@@ -0,0 +1,5 @@
+export interface CardMainInfoProps {
+  type?: string;
+  title: string;
+}
+
--- src/components/Card/PostingCard.tsx
+++ src/components/Card/PostingCard.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Card, CardContent, CardMedia } from '@mui/material';
+import { Typography } from '@material-tailwind/react';
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
+import { CardMainInfoProps } from './PostingCard.interface';
 
-interface PostingCardProps {
+interface PostingCardProps extends CardMainInfoProps {
   thumbnailPath: string | null;
 }
 
-const PostingCard = ({ thumbnailPath }: PostingCardProps) => {
+const CardMainInfo = ({ type, title }: CardMainInfoProps) => {
+  return (
+    <div>
+      <Typography className="-mt-2 h-3 font-medium text-pointBlue" variant="small">
+        {type ?? ''}
+      </Typography>
+      <Typography className="font-semibold" variant="paragraph">
+        {title}
+      </Typography>
+    </div>
+  );
+};
+
+const PostingCard = ({ thumbnailPath, type, title }: PostingCardProps) => {
   return (
     <Card className="w-52 !rounded-none !bg-middleBlack !bg-none">
       {thumbnailPath ? (
@@ -14,7 +29,9 @@ const PostingCard = ({ thumbnailPath }: PostingCardProps) => {
       ) : (
         <Logo className="m-auto h-[118px] w-28" />
       )}
-      <CardContent className="h-24 !bg-mainBlack" />
+      <CardContent className="h-24 !bg-mainBlack">
+        <CardMainInfo type={type} title={title} />
+      </CardContent>
     </Card>
   );
 };
