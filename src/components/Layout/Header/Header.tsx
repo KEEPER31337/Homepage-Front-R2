0410c1208b7f1bd2b7680a2ed3b1c0365f90a421
publdaze <publdaze@naver.com>
2023-02-28 15:03:59 +0900

    feat : 계정 정보 메뉴 띄워주는 기능 추가
    #103

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -3,8 +3,19 @@ import { Link } from 'react-router-dom';
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
 import { VscGithubInverted, VscAccount } from 'react-icons/vsc';
+import AccountMenu from './Menu/AccountMenu';
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleAccountIconClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed" className="h-header border-b border-pointBlue !bg-mainBlack !bg-none">
       <Toolbar className="flex justify-between">
@@ -18,11 +29,12 @@ const Header = () => {
           <IconButton target="_blank" href="https://github.com/KEEPER31337">
             <VscGithubInverted fill="#4CEEF9" />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={handleAccountIconClick}>
             <VscAccount fill="#4CEEF9" />
           </IconButton>
         </div>
       </Toolbar>
+      <AccountMenu anchorEl={anchorEl} open={open} onClose={handleMenuClose} />
     </AppBar>
   );
 };
