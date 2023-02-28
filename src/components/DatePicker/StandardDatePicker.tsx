74f0b78882a552149a46039116d7c8b0a5565791
pipisebastian <yrt7998@pusan.ac.kr>
2023-02-28 22:33:06 +0900

    feat : mui TextFiled 중괄호 사용하여 import
    #213

--- src/components/DatePicker/StandardDatePicker.tsx
+++ src/components/DatePicker/StandardDatePicker.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { DateTime } from 'luxon';
-import TextField from '@mui/material/TextField';
+import { TextField } from '@mui/material';
 import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 
