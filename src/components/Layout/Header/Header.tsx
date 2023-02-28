73cb6fb4ebf18188200583ee2fd643033fa6e277
publdaze <publdaze@naver.com>
2023-02-28 13:06:28 +0900

    feat : 헤더에 로고 추가
    - 로고 클릭 시 홈으로 이동하도록 처리
    
    #103

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { AppBar, Toolbar } from '@mui/material';
+import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
 
 const Header = () => {
   return (
     <AppBar position="fixed" className="h-header border-b border-pointBlue !bg-mainBlack !bg-none">
-      <Toolbar />
+      <Toolbar className="flex justify-between">
+        <Link to="/">
+          <Logo className="h-8" />
+        </Link>
+      </Toolbar>
     </AppBar>
   );
 };
