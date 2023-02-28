84536b1b1eeb2d81a0f66856d1dc0c90d81062db
publdaze <publdaze@naver.com>
2023-02-28 15:01:03 +0900

    feat : 우측 아이콘 버튼들 추가
    - 위키 새 탭에 링크 열리도록 구현
    - 키퍼 깃헙 Organ 새탭에 링크 열리도록 구현
    
    #103

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
+import { VscGithubInverted, VscAccount } from 'react-icons/vsc';
 
 const Header = () => {
   return (
@@ -10,6 +11,17 @@ const Header = () => {
         <Link to="/">
           <Logo className="h-8" />
         </Link>
+        <div>
+          <IconButton target="_blank" href="https://keeper.or.kr/wiki/%EB%8C%80%EB%AC%B8">
+            <Typography className=" h-6 w-6 rounded-full bg-pointBlue text-mainBlack">W</Typography>
+          </IconButton>
+          <IconButton target="_blank" href="https://github.com/KEEPER31337">
+            <VscGithubInverted fill="#4CEEF9" />
+          </IconButton>
+          <IconButton>
+            <VscAccount fill="#4CEEF9" />
+          </IconButton>
+        </div>
       </Toolbar>
     </AppBar>
   );
