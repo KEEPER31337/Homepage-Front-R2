99e4761bdc1e6a0e4be3f47cd22cad81b7d71984
publdaze <publdaze@naver.com>
2023-03-01 22:15:48 +0900

    refactor : 단순 텍스트의 경우 p 태그에서 Typography로 변경
    - 텍스트 사용시  텍스트 전체적으로 변경되는 사항 있을 경우 공통적으로 처리해주기 위해서 Typography 컴포넌트로 적용
    - tailwindcss 정의된 클래스 사이즈 맞춰주기
    
    #153

--- src/pages/Study/StudyModal.tsx
+++ src/pages/Study/StudyModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VscGithubInverted, VscLink } from 'react-icons/vsc';
 import { SiNotion } from 'react-icons/si';
+import { Typography } from '@material-tailwind/react';
 
 import ActionModal from '@components/Modal/ActionModal';
 import StudyInput from './share/StudyInput';
@@ -31,22 +32,22 @@ const StudyModal = ({ open, isModify, handleOpen, studyId }: StudyModalProps): J
             <StudyTextarea variant="static" label="스터디 소개" placeholder="스터디 소개를 적어주세요." />
           </div>
           <div className="w-[141px] space-y-[10px]">
-            <p className="text-pointBlue">썸네일</p>
+            <Typography className="text-pointBlue">썸네일</Typography>
             <div className="flex h-[130px] w-[130px] items-center justify-center rounded-[10px] border-2 border-dashed border-pointBlue/30 text-pointBlue/30">
               aaa
             </div>
           </div>
         </div>
-        <div className="flex space-x-[16px] ">
-          <div className="w-[108px]  space-y-[4px]">
-            <p className="text-pointBlue">스터디장</p>
-            <div className="flex border-b-[2px] border-pointBlue py-[6px]">
+        <div className="flex space-x-4">
+          <div className="w-[108px] space-y-1">
+            <Typography className="text-pointBlue">스터디장</Typography>
+            <div className="flex border-b-2 border-pointBlue py-[6px]">
               <StudyChip value="박재열" />
             </div>
           </div>
-          <div className="w-full space-y-[4px]">
-            <p className="text-pointBlue">스터디원</p>
-            <div className="flex space-x-[4px] border-b-[2px] border-pointBlue py-[6px]">
+          <div className="w-full space-y-1">
+            <Typography className="text-pointBlue">스터디원</Typography>
+            <div className="flex space-x-1 border-b-2 border-pointBlue py-[6px]">
               <StudyChip value="박재열" />
               {memberList.map((member) => (
                 <StudyChipDismissible
@@ -62,21 +63,20 @@ const StudyModal = ({ open, isModify, handleOpen, studyId }: StudyModalProps): J
             </div>
           </div>
         </div>
-        <div className="space-y-[16px]">
-          <p className="text-pointBlue">링크</p>
-
-          <div className="space-y-[16px]">
-            <div className="flex space-x-[8px]">
+        <div className="space-y-4">
+          <Typography className="text-pointBlue">링크</Typography>
+          <div className="space-y-4">
+            <div className="flex space-x-2">
               <VscGithubInverted className="h-[25px] w-[25px] text-pointBlue" />
               <StudyInput size="md" variant="static" label="Github" placeholder="https://" />
             </div>
-            <div className="flex space-x-[8px]">
+            <div className="flex space-x-2">
               <SiNotion className="h-[25px] w-[25px] text-pointBlue" />
               <StudyInput size="md" variant="static" label="Notion" placeholder="https://" />
             </div>
-            <div className="flex space-x-[8px]">
+            <div className="flex space-x-2">
               <VscLink className="h-[25px] w-[25px] text-pointBlue" />
-              <div className="flex w-full space-x-[16px]">
+              <div className="flex w-full space-x-4">
                 <p className="w-[90px]">
                   <StudyInput size="md" variant="static" label="etc." placeholder="ex)Plato" />
                 </p>
