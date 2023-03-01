295cd64b9cda92f69b38a6159eba224860d23d05
publdaze <publdaze@naver.com>
2023-03-01 21:09:07 +0900

    refactor : 임시 레이아웃 제거
    #153

--- src/pages/Study/Study.tsx
+++ src/pages/Study/Study.tsx
@@ -13,30 +13,27 @@ const Study = () => {
   const [currentSeason, setCurrentSeason] = useState('1학기');
 
   return (
-    <div className="flex w-full justify-center">
-      <div className="h-full w-full max-w-[1080px] border pt-[80px]">
-        <PageTitle>스터디</PageTitle>
-
-        <div className="h-full w-full space-y-[32px]">
-          <div className="flex h-[45px] items-center justify-between">
-            <div className="flex space-x-[8px]">
-              <StudySelect value="2022" options={yearList} setValue={setCurrentYear} />
-              <StudySelect value="1학기" options={seasonList} setValue={setCurrentSeason} />
-            </div>
-            {currentYear !== '2021' ? <StudyModal isModify={false} studyId={undefined} /> : ''}
+    <div>
+      <PageTitle>스터디</PageTitle>
+      <div className="h-full w-full space-y-[32px]">
+        <div className="flex h-[45px] items-center justify-between">
+          <div className="flex space-x-[8px]">
+            <StudySelect value="2022" options={yearList} setValue={setCurrentYear} />
+            <StudySelect value="1학기" options={seasonList} setValue={setCurrentSeason} />
           </div>
-          {currentYear !== '2021' ? (
-            <OldStudy studyList={studyList} />
-          ) : (
-            <div className="">
-              {studyList?.map((study) => (
-                <Fragment key={study}>
-                  <StudyItem id={study} />
-                </Fragment>
-              ))}
-            </div>
-          )}
+          {currentYear !== '2021' ? <StudyModal isModify={false} studyId={undefined} /> : ''}
         </div>
+        {currentYear !== '2021' ? (
+          <OldStudy studyList={studyList} />
+        ) : (
+          <div className="">
+            {studyList?.map((study) => (
+              <Fragment key={study}>
+                <StudyItem id={study} />
+              </Fragment>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
