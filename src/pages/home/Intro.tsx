6658274ab30a99b3bfceee99af5abf7c25dea31e
jasper200207 <jasper12394@gmail.com>
2023-03-02 14:34:59 +0900

    refactor : Intro 높이 h-screen으로 설정

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
     "'지키다'라는 의미를 가진 단어 'keep'에서 착안하여,\n정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다.";
 
   return (
-    <div className="flex h-[calc(100vh-theme(space.header))] w-full flex-col place-content-center place-items-center">
+    <div className="flex h-screen w-full flex-col place-content-center place-items-center">
       <div className="space-y-24">
         <LogoNeon className="w-[700px]" />
         <Typography variant="h3" className="whitespace-pre-wrap text-center">
