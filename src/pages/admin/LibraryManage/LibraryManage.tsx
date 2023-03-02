389b235eb975ebd5bf3dbc0e8709d592ef647a68
pipisebastian <yrt7998@pusan.ac.kr>
2023-03-03 00:32:21 +0900

    style : tailwind 필요없는 코드 제거
    - div 2중 1개로 줄임
    - 필요없는 w-fit, h-fit제거
    
    #145

--- src/pages/admin/LibraryManage/LibraryManage.tsx
+++ src/pages/admin/LibraryManage/LibraryManage.tsx
@@ -12,11 +12,7 @@ const LibraryManage = () => {
   const childComponent = ({ key, value }: ChildComponent<BookListInfo>) => {
     switch (key) {
       case 'enable':
-        return value ? (
-          <div className="h-fit w-fit text-pointBlue">대출가능</div>
-        ) : (
-          <div className="h-fit w-fit text-subGray">대출불가</div>
-        );
+        return value ? <div className="text-pointBlue">대출가능</div> : <div className="text-subGray">대출불가</div>;
       default:
         return value;
     }
@@ -25,18 +21,16 @@ const LibraryManage = () => {
   return (
     <div>
       <PageTitle>도서관리</PageTitle>
-      <div className="flex w-full flex-col space-y-4">
-        <div className="flex w-full justify-between">
-          <SearchSection />
-          <div className="flex space-x-2">
-            <OutlinedButton>도서 추가</OutlinedButton>
-            <OutlinedButton>도서 삭제</OutlinedButton>
-            <OutlinedButton>대출 관리</OutlinedButton>
-            <OutlinedButton>반납 관리</OutlinedButton>
-          </div>
+      <div className="mb-5 flex w-full items-center justify-between ">
+        <SearchSection />
+        <div className="flex space-x-2">
+          <OutlinedButton>도서 추가</OutlinedButton>
+          <OutlinedButton>도서 삭제</OutlinedButton>
+          <OutlinedButton>대출 관리</OutlinedButton>
+          <OutlinedButton>반납 관리</OutlinedButton>
         </div>
-        <StandardTable<BookListInfo> columns={columns} rows={rows} childComponent={childComponent} />
       </div>
+      <StandardTable<BookListInfo> columns={columns} rows={rows} childComponent={childComponent} />
     </div>
   );
 };
