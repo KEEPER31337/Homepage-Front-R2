adba9fed9ba7d6c0390ecc936dc33d5e52f7178a
pipisebastian <yrt7998@pusan.ac.kr>
2023-03-03 22:49:53 +0900

    feat:  props spread 연산자 사용
    #279

--- src/components/Input/StandardInput.tsx
+++ src/components/Input/StandardInput.tsx
@@ -1,39 +1,15 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { TextField } from '@mui/material';
+import { TextField, StandardTextFieldProps } from '@mui/material';
 
-interface StandardInputProps {
-  className?: string;
-  label?: string;
-  disabled?: boolean;
-  error?: boolean;
-  helperText?: string;
+interface StandardInputProps extends StandardTextFieldProps {
   name: string;
   value: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
-const StandardInput = ({
-  className,
-  label,
-  disabled,
-  error,
-  name,
-  helperText,
-  value,
-  onChange,
-}: StandardInputProps) => {
-  return (
-    <TextField
-      variant="standard"
-      className={className}
-      label={label}
-      disabled={disabled}
-      error={error}
-      helperText={helperText}
-      name={name}
-      value={value}
-      onChange={onChange}
-    />
-  );
+
+const StandardInput = ({ name, value, onChange, ...standardTextFieldProps }: StandardInputProps) => {
+  return <TextField name={name} value={value} onChange={onChange} {...standardTextFieldProps} variant="standard" />;
 };
 
 export default StandardInput;
