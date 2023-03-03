a4a83a19be07982fefec318be49e5dc2d91af2c5
pipisebastian <yrt7998@pusan.ac.kr>
2023-03-03 23:27:48 +0900

    feat: 검색아이콘 IconButton컴포넌트로 감싸기, 검색아이콘 css 수정
    - 검색아이콘 크기 size로 조정하도록
    - 색깔 fill로 채우도록
    
    #240

--- src/components/Input/SearchInput.tsx
+++ src/components/Input/SearchInput.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { KeyboardEvent } from 'react';
-import { TextField, InputAdornment, StandardTextFieldProps } from '@mui/material';
+import { TextField, InputAdornment, StandardTextFieldProps, IconButton } from '@mui/material';
 import { MdOutlineSearch } from 'react-icons/md';
 
 interface SearchInputProps extends StandardTextFieldProps {
@@ -23,7 +23,9 @@ const SearchInput = ({ value, onChange, onActionSearch, ...standardTextFieldProp
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
-            <MdOutlineSearch className="h-5 w-5 text-pointBlue hover:cursor-pointer" onClick={onActionSearch} />
+            <IconButton onClick={onActionSearch}>
+              <MdOutlineSearch size={20} className="fill-pointBlue" />
+            </IconButton>
           </InputAdornment>
         ),
       }}
