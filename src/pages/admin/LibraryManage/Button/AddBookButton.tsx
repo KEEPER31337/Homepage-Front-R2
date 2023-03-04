5924e1f162ffe2e0903021c3da043de82e6b4ba4
pipisebastian <yrt7998@pusan.ac.kr>
2023-03-04 21:23:15 +0900

    feat: 도서추가시 필요한 inputfield(도서명, 저자) 생성
    #220

--- src/pages/admin/LibraryManage/Button/AddBookButton.tsx
+++ src/pages/admin/LibraryManage/Button/AddBookButton.tsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
-import { Input } from '@material-tailwind/react';
+import { BookListInfo } from '@api/dto';
 
 import OutlinedButton from '@components/Button/OutlinedButton';
 import ActionModal from '@components/Modal/ActionModal';
 
+import StandardInput from '@components/Input/StandardInput';
+
+type AddBookInfo = Pick<BookListInfo, 'title' | 'author'>;
+
 const AddBookButton = () => {
   const [addBookModal, setAddBookModal] = useState(false);
   const handleAddBookModal = () => setAddBookModal(!addBookModal);
+
+  const [form, setForm] = React.useState<AddBookInfo>({
+    title: '',
+    author: '',
+  });
+  const { title, author } = form;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   return (
     <div className="flex">
@@ -18,13 +36,13 @@ const AddBookButton = () => {
         title="도서추가"
         buttonName="추가"
         onActionButonClick={() => {
-          console.log('도서 추가 api');
+          console.log(title, author);
         }}
       >
-        <div className="flex h-fit w-full flex-row space-x-2 ">
-          <Input label="도서명" className="flex w-64" />
-          <Input label="저자명" className="flex w-64" />
-          <Input label="권수" className="flex w-14" />
+        <div className="flex h-full w-full flex-col space-y-2 ">
+          <StandardInput name="title" value={title} onChange={onChange} label="도서명" />
+          <StandardInput name="author" value={author} onChange={onChange} label="저자명" />
+          {/* <StandardInput name="total" value={total} onChange={onChange} label="권수" /> */}
         </div>
       </ActionModal>
     </div>
