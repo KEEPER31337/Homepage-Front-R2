e55d6df9bbda59bb187f796318de3e10d57d170f
pipisebastian <yrt7998@pusan.ac.kr>
2023-03-05 18:30:56 +0900

    feat: state대신 useReducer로 모달창 상태여부 관리
    #220

--- src/pages/admin/LibraryManage/Button/DeleteBookButton.tsx
+++ src/pages/admin/LibraryManage/Button/DeleteBookButton.tsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import OutlinedButton from '@components/Button/OutlinedButton';
 import ActionModal from '@components/Modal/ActionModal';
 
 const DeleteBookButton = () => {
-  const [deleteBookModal, setDeleteBookModal] = useState(false);
-  const handleDeleteBookModal = () => setDeleteBookModal(!deleteBookModal);
-
+  const [open, toggleOpen] = useReducer((prev) => !prev, false);
   return (
     <div className="flex">
-      <OutlinedButton onClick={() => handleDeleteBookModal()}>도서 삭제</OutlinedButton>
+      <OutlinedButton onClick={() => toggleOpen()}>도서 삭제</OutlinedButton>
 
       <ActionModal
-        opened={deleteBookModal}
-        handleOpen={handleDeleteBookModal}
+        opened={open}
+        handleOpen={toggleOpen}
         title="도서삭제"
         buttonName="삭제"
         onActionButonClick={() => {
-          console.log('도서 삭제 api');
+          toggleOpen();
         }}
       >
         N권의 도서를 삭제하시겠습니까?
