9218d67ee3b2deb049ef1c50b38239a1fa36dbe8
mia2583 <mia2583@pusan.ac.kr>
2023-03-06 21:58:15 +0900

    style : prop의 이벤트 핸들러 네이밍 수정 - prop의 경우 on을 접두사로 붙여주기

--- src/components/Modal/ConfirmModal.tsx
+++ src/components/Modal/ConfirmModal.tsx
@@ -6,15 +6,15 @@ import { VscChromeClose } from 'react-icons/vsc';
 
 interface ConfirmModalProps {
   opened: boolean;
-  handleOpen: () => void;
+  onHandleOpen: () => void;
   title: string;
   children: React.ReactNode;
 }
 
-const ConfirmModal = ({ opened, handleOpen, title, children }: ConfirmModalProps) => {
+const ConfirmModal = ({ opened, onHandleOpen, title, children }: ConfirmModalProps) => {
   return (
-    <Dialog open={opened} handler={handleOpen} className="rounded-lg bg-subBlack py-5 px-10">
-      <VscChromeClose className="absolute right-5" size="19" color="#4CEEF9" onClick={handleOpen} />
+    <Dialog open={opened} handler={onHandleOpen} className="rounded-lg bg-subBlack py-5 px-10">
+      <VscChromeClose className="absolute right-5" size="19" color="#4CEEF9" onClick={onHandleOpen} />
       <DialogHeader className="break-all font-bold text-pointBlue">
         <Typography variant="h3">{title}</Typography>
       </DialogHeader>
