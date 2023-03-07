268a4eff66a3b7c9992e5742025eeda7ad7c8eaa
publdaze <publdaze@naver.com>
2023-03-07 16:02:41 +0900

    design : mui 라이브러리에 맞춰서 스타일 재조정
    #287

--- src/components/Modal/ActionModal.tsx
+++ src/components/Modal/ActionModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
-import { Typography } from '@material-tailwind/react';
 import FilledButton from '@components/Button/FilledButton';
 import TextButton from '@components/Button/TextButton';
 
@@ -15,17 +14,13 @@ interface ActionModalProps {
 
 const ActionModal = ({ opened, handleOpen, title, children, buttonName, onActionButonClick }: ActionModalProps) => {
   return (
-    <Dialog open={opened} className="rounded-lg bg-subBlack">
-      <div className="mt-5 ml-10 mb-2 mr-10">
-        <DialogTitle component="div" className="break-all text-pointBlue">
-          <Typography variant="h3">{title}</Typography>
-        </DialogTitle>
-        <DialogContent className="mb-20 break-all text-white">{children}</DialogContent>
-        <DialogActions className="absolute right-1 bottom-1">
-          <TextButton onClick={handleOpen}>취소</TextButton>
-          <FilledButton onClick={onActionButonClick}>{buttonName}</FilledButton>
-        </DialogActions>
-      </div>
+    <Dialog open={opened}>
+      <DialogTitle className="text-pointBlue">{title}</DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions>
+        <TextButton onClick={handleOpen}>취소</TextButton>
+        <FilledButton onClick={onActionButonClick}>{buttonName}</FilledButton>
+      </DialogActions>
     </Dialog>
   );
 };
