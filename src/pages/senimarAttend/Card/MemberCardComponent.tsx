a287f326370098be043e38af0ea1a0893c2401ec
lcqff <gooyeonhu82@gmail.com>
2023-03-09 18:10:37 +0900

    design: 비활성화 경우 카드 반투명처리, 카드 background 컬러 수정

--- src/pages/senimarAttend/Card/MemberCardComponent.tsx
+++ src/pages/senimarAttend/Card/MemberCardComponent.tsx
@@ -6,17 +6,14 @@ import SeminarInput from '../Input/SeminarInput';
 
 const MemberCardComponent = () => {
   const [componentIncorrectAlert, setComponentIncorrectAlert] = useState(<p className="mb-[22px]" />);
-  const [seminarExist, setSeminarExist] = useState(false); // TODO: api로 교체
 
-  return seminarExist ? (
+  return (
     <>
       <SeminarInput disabled={false} />
       <div className="mx-auto flex items-center justify-center text-small text-red-500">{componentIncorrectAlert}</div>
       <SeminarButton>출석</SeminarButton>
       <Countdown />
     </>
-  ) : (
-    <Typography className="text-center text-h3 font-bold">세미나 비활성화 상태</Typography>
   );
 };
 
--- src/pages/senimarAttend/Card/SeminarCard.tsx
+++ src/pages/senimarAttend/Card/SeminarCard.tsx
@@ -1,12 +1,19 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import { Card, CardContent } from '@mui/material';
 
 interface SeminarCardProps {
   children: ReactNode;
 }
+
 const SeminarCard = ({ children }: SeminarCardProps) => {
+  const [seminarExist, setSeminarExist] = useState(false); // TODO: api로 교체
+
   return (
-    <Card className="flex h-[426px] w-96 items-center justify-center bg-mainBlack">
+    <Card
+      className={`flex h-[426px] w-96 items-center justify-center !bg-middleBlack !bg-none ${
+        seminarExist ? 'opacity-50' : ''
+      }`}
+    >
       <CardContent className="h-[315px]">{children}</CardContent>
     </Card>
   );
