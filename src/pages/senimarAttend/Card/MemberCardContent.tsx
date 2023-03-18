623027666828a34add7153eed1abb8f34cadf324
lcqff <gooyeonhu82@gmail.com>
2023-03-18 17:15:55 +0900

    refactor: state값 dom에서 text로 바꾸기

--- src/pages/senimarAttend/Card/MemberCardContent.tsx
+++ src/pages/senimarAttend/Card/MemberCardContent.tsx
@@ -5,8 +5,8 @@ import SeminarButton from '../Button/SeminarButton';
 import SeminarInput from '../Input/SeminarInput';
 
 const MemberCardContent = () => {
-  const [componentIncorrectAlert, setComponentIncorrectAlert] = useState(<p className="mb-[22px]" />);
-  const incorrectCodeAlert = <p className="my-[4px] ">출석코드가 맞지 않습니다. 다시 입력해주세요.</p>;
+  const [componentIncorrectAlert, setComponentIncorrectAlert] = useState('');
+  const incorrectCodeAlert = '출석코드가 맞지 않습니다. 다시 입력해주세요.';
   const attendLimit = new Date(); // 임시
   attendLimit.setMinutes(attendLimit.getMinutes() + 5); // 임시
   const inputCode = [0, 0, 0, 0];
@@ -15,13 +15,13 @@ const MemberCardContent = () => {
     if (today < attendLimit) {
       // 임시: api에서 출석 status 가져올수 있으면 if문 필요없음
       if (inputCode.join('') === validCode) {
-        setComponentIncorrectAlert(<p />);
+        setComponentIncorrectAlert('');
       } else {
         setComponentIncorrectAlert(incorrectCodeAlert);
         // TODO: Input 내용물 전부 지우기
       }
     } else {
-      setComponentIncorrectAlert(<p />);
+      setComponentIncorrectAlert('');
     }
   };
 
