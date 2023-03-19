b610d798c1d394ebed5f4a133828b313722b7223
publdaze <publdaze@naver.com>
2023-03-19 15:00:27 +0900

    feat : 단계 안내 문구 구현
    #292

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import StepProgress from '@components/Progress/StepProgress';
 
 const SignUp = () => {
   const TOTAL_STEPS = 3;
 
   const currentStep = 1; // TODO 추후 state로 관리
+  const stepInfoMsg = {
+    1: '로그인에 사용할\n아이디와 비밀번호를 등록해 주세요.',
+    2: '프로필 정보를 등록해 주세요.',
+    3: '이메일 주소를 입력해주세요.\n입력한 이메일 주소로 인증 코드가 발송됩니다.',
+  };
 
   return (
     <div className="grid h-screen place-content-center place-items-center">
       <Logo className="mb-9 w-48" />
       <Box className="h-[492px] w-[690px] border border-pointBlue px-24 py-14">
-        <StepProgress className="w-32" currentStep={currentStep} totalStep={TOTAL_STEPS} />
+        <StepProgress className="mb-2 w-32" currentStep={currentStep} totalStep={TOTAL_STEPS} />
+        <Typography className="whitespace-pre !font-semibold">{stepInfoMsg[currentStep]}</Typography>
       </Box>
     </div>
   );
