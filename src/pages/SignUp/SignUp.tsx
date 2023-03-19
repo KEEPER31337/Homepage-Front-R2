1c14ad13e7f6790b824b4e966935e097300584ec
publdaze <publdaze@naver.com>
2023-03-19 14:31:27 +0900

    feat : 회원가입 단계별 progressbar UI 구성
    #292

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
 import { Box } from '@mui/material';
+import StepProgress from '@components/Progress/StepProgress';
 
 const SignUp = () => {
+  const TOTAL_STEPS = 3;
+
+  const currentStep = 1; // TODO 추후 state로 관리
+
   return (
     <div className="grid h-screen place-content-center place-items-center">
       <Logo className="mb-9 w-48" />
-      <Box className="h-[492px] w-[690px] border border-pointBlue px-24 py-14" />
+      <Box className="h-[492px] w-[690px] border border-pointBlue px-24 py-14">
+        <StepProgress currentStep={currentStep} totalStep={TOTAL_STEPS} />
+      </Box>
     </div>
   );
 };
