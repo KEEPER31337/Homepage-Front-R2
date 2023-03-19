a86015b82ff75e1a1a2060ce53182695bd8c872d
publdaze <publdaze@naver.com>
2023-03-19 14:56:37 +0900

    feat : progressbar에 className 받아올 수 있도록 확장
    #292

--- src/components/Progress/StepProgress.tsx
+++ src/components/Progress/StepProgress.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { LinearProgress } from '@mui/material';
 
 interface StepProgress {
+  className?: string;
   currentStep: number;
   totalStep: number;
 }
 
-const StepProgress = ({ currentStep, totalStep }: StepProgress) => {
+const StepProgress = ({ className, currentStep, totalStep }: StepProgress) => {
   const currentProgressRate = (100 / totalStep) * currentStep;
 
-  return <LinearProgress className="w-32" variant="determinate" value={currentProgressRate} />;
+  return <LinearProgress className={className} variant="determinate" value={currentProgressRate} />;
 };
 
 export default StepProgress;
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -13,7 +13,7 @@ const SignUp = () => {
     <div className="grid h-screen place-content-center place-items-center">
       <Logo className="mb-9 w-48" />
       <Box className="h-[492px] w-[690px] border border-pointBlue px-24 py-14">
-        <StepProgress currentStep={currentStep} totalStep={TOTAL_STEPS} />
+        <StepProgress className="w-32" currentStep={currentStep} totalStep={TOTAL_STEPS} />
       </Box>
     </div>
   );
