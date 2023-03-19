ee05798ff8b5ffa6462d0e72130a350d0ef52f06
publdaze <publdaze@naver.com>
2023-03-19 13:58:22 +0900

    feat : 외곽 박스 틀 생성
    #292

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
+import { Box } from '@mui/material';
 
 const SignUp = () => {
   return (
-    <div className="grid h-screen place-content-center">
-      <Logo className="w-48" />
+    <div className="grid h-screen place-content-center place-items-center">
+      <Logo className="mb-9 w-48" />
+      <Box className="h-[492px] w-[690px] border border-pointBlue px-24 py-14" />
     </div>
   );
 };
