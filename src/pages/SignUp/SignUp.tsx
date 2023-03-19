c271b79d1c14bfed919f7a93da263cdb7e112e48
publdaze <publdaze@naver.com>
2023-03-19 13:50:11 +0900

    feat : 회원가입 UI 로고 추가
    #292

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
+
 const SignUp = () => {
-  return <div />;
+  return (
+    <div className="grid h-screen place-content-center">
+      <Logo className="w-48" />
+    </div>
+  );
 };
 
 export default SignUp;
