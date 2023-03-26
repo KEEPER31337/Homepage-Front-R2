4723fa76b9d59b45dbd38c9196bc81e86a0b58b6
lcqff <gooyeonhu82@gmail.com>
2023-03-26 21:33:54 +0900

    feat: input 입력시 자동으로 다음칸으로 이동시킴
    #309

--- src/pages/senimarAttend/Input/SeminarInput.tsx
+++ src/pages/senimarAttend/Input/SeminarInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const inputStyle: React.CSSProperties = {
   textAlign: 'center',
@@ -16,11 +16,48 @@ interface SeminarInputProps {
 }
 const SeminarInput = ({ disabled, helperText }: SeminarInputProps) => {
   const inputListKey = [0, 1, 2, 3];
+  const focusInput0 = useRef<HTMLInputElement>(null);
+  const focusInput1 = useRef<HTMLInputElement>(null);
+  const focusInput2 = useRef<HTMLInputElement>(null);
+  const focusInput3 = useRef<HTMLInputElement>(null);
+  const inputList = [focusInput0, focusInput1, focusInput2, focusInput3];
+  const inputCode = [0, 0, 0, 0];
+
+  const moveNextInput = (value: string, index: number) => {
+    if (value) {
+      inputCode[index] = Number(value);
+      switch (index) {
+        case 0:
+          focusInput1.current?.focus();
+          break;
+        case 1:
+          focusInput2.current?.focus();
+          break;
+        case 2:
+          focusInput3.current?.focus();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
   return (
     <div className="mx-auto my-[22px]">
       <div className="flex h-[52px] w-[192px] justify-between">
         {inputListKey.map((key) => {
-          return <input key={key} style={inputStyle} maxLength={1} disabled={disabled} />;
+          return (
+            <input
+              key={key}
+              style={inputStyle}
+              maxLength={1}
+              disabled={disabled}
+              onChange={(e) => {
+                moveNextInput(e.target.value, key);
+              }}
+              ref={inputList[key]}
+            />
+          );
         })}
       </div>
       <div className="my-[4px] flex items-center justify-center text-small text-red-500">{helperText}</div>
