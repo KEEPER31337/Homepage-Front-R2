ecaaf1e1d88dbb527eca4b59dddc1b1dea6a32dc
mia2583 <mia2583@pusan.ac.kr>
2023-03-27 16:42:01 +0900

    style : 아이콘 색상, 사이즈 적용 스타일 수정 - ""대신 {}로 사이즈 지정 - 색상을 color가 아닌 className에서 적용
    # 290

--- src/components/Modal/ConfirmModal.tsx
+++ src/components/Modal/ConfirmModal.tsx
@@ -13,7 +13,7 @@ interface ConfirmModalProps {
 const ConfirmModal = ({ open, onClose, modalWidth, title, children }: ConfirmModalProps) => {
   return (
     <Dialog open={open} PaperProps={{ className: 'px-2 py-1' }} fullWidth={Boolean(modalWidth)} maxWidth={modalWidth}>
-      <VscChromeClose className="absolute right-0 m-5 cursor-pointer" size="19" color="#4CEEF9" onClick={onClose} />
+      <VscChromeClose className="absolute right-0 m-5 cursor-pointer fill-pointBlue" size={19} onClick={onClose} />
       <DialogTitle className="!font-bold text-pointBlue">{title}</DialogTitle>
       <DialogContent className="min-h-[80px] min-w-[350px]">{children}</DialogContent>
     </Dialog>
