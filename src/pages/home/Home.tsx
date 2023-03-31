7e3d0437c3ce0c653f15302c055f54b86480c5da
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2023-03-31 14:31:04 +0900

    refactor : Home에 import 제외
    - 나중에 머지할때 충돌 대비
    #121

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,10 @@
-import { useGetTitleTypesQuery } from '@api/aboutApi';
 import React from 'react';
-import Activity from './Activity';
 import Intro from './Intro';
 
 const Home = () => {
   return (
     <div className="flex w-full flex-col bg-galaxy bg-contain bg-repeat">
       <Intro />
-      <Activity />
     </div>
   );
 };
