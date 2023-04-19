91caecdc49937d474dfb64b01e48b47ddf69ef19
amaran-th <songsy405@gmail.com>
2023-04-19 18:24:46 +0900

    feat : 스터디 수정/삭제 버튼을 AccodiaonHeader로 이동
    #154

--- src/pages/Study/Accordion/StudyAccordion.tsx
+++ src/pages/Study/Accordion/StudyAccordion.tsx
@@ -18,15 +18,33 @@ interface StudyAccordionProps {
   setModalInfo: Dispatch<SetStateAction<ModalInfo>>;
 }
 
-type StudyAccordionHeaderProps = Pick<StudyAccordionProps, 'study'>;
+type StudyAccordionBodyProps = Pick<StudyAccordionProps, 'study'>;
 
-const StudyAccordionHeader = ({ study }: StudyAccordionHeaderProps) => {
+const StudyAccordionHeader = ({ study, memberId, toggleOpen, setModalInfo }: StudyAccordionProps) => {
+  const { headMember } = study;
+
+  const handleStudyModifyButtonClick = () => {
+    toggleOpen();
+    setModalInfo({ mode: 'Modify', selectedStudy: study });
+  };
+  const handleStudyDeleteButtonClick = () => {
+    // TODO 스터디 제거 API 호출 후 새로고침(기능 구현 후 console 삭제 예정)
+    console.log(`${study.title}삭제`);
+  };
+
   return (
     <div className="flex w-full space-x-2 pl-2 text-left">
       <span className="max-w-16 min-w-16 h-16 max-h-16 w-16 bg-gray-300">이미지</span>
       <div className="flex w-full items-center justify-between space-x-2 pr-2">
         <Typography className="text-h3 font-bold">{study.title}</Typography>
+
         <div className="flex items-center space-x-2">
+          {memberId === headMember.id && (
+            <div className="space-x-2 pr-2">
+              <OutlinedButton onClick={handleStudyModifyButtonClick}>수정</OutlinedButton>
+              <OutlinedButton onClick={handleStudyDeleteButtonClick}>삭제</OutlinedButton>
+            </div>
+          )}
           <Typography className="font-semibold">스터디장</Typography>
           <StudyChip font="Semibold" value={study.headMember.realName} />
           <Divider className="!border-white" orientation="vertical" flexItem />
@@ -39,26 +57,17 @@ const StudyAccordionHeader = ({ study }: StudyAccordionHeaderProps) => {
   );
 };
 
-const StudyAccordionBody = ({ study, memberId, toggleOpen, setModalInfo }: StudyAccordionProps) => {
+const StudyAccordionBody = ({ study }: StudyAccordionBodyProps) => {
   interface IconType {
     [key: string]: React.ReactNode;
   }
 
-  const { information, headMember, memberList } = study;
+  const { information, memberList } = study;
 
   const LinkIconData: IconType = {
     github: <VscGithubInverted className="h-5 w-5" />,
     notion: <SiNotion className="h-5 w-5" />,
     etc: <VscLink className="h-5 w-5" />,
-  };
-
-  const handleStudyModifyButtonClick = () => {
-    toggleOpen();
-    setModalInfo({ mode: 'Modify', selectedStudy: study });
-  };
-  const handleStudyDeleteButtonClick = () => {
-    // TODO 스터디 제거 API 호출 후 새로고침(기능 구현 후 console 삭제 예정)
-    console.log(`${study.title}삭제`);
   };
 
   return (
@@ -68,12 +77,6 @@ const StudyAccordionBody = ({ study, memberId, toggleOpen, setModalInfo }: Study
           <Typography className="font-semibold">스터디 소개</Typography>
           <Typography className="border-l-2 border-pointBlue px-2">{information}</Typography>
         </div>
-        {memberId === headMember.id && (
-          <div className="space-x-2">
-            <OutlinedButton onClick={handleStudyModifyButtonClick}>수정</OutlinedButton>
-            <OutlinedButton onClick={handleStudyDeleteButtonClick}>삭제</OutlinedButton>
-          </div>
-        )}
       </div>
       <div className="space-y-4">
         <Typography className="font-semibold">스터디원</Typography>
@@ -105,10 +108,10 @@ const StudyAccordion = ({ study, memberId, toggleOpen, setModalInfo }: StudyAcco
         className="!h-20 !border-b !border-white/[20%] !bg-subBlack !px-4 !text-white hover:!bg-subGray hover:!text-white focus:!outline-0"
         expandIcon={<VscChevronDown />}
       >
-        <StudyAccordionHeader study={study} />
+        <StudyAccordionHeader study={study} memberId={memberId} toggleOpen={toggleOpen} setModalInfo={setModalInfo} />
       </AccordionSummary>
       <AccordionDetails className="!space-y-[30px] !bg-middleBlack !py-[30px] !px-[41px] !text-white">
-        <StudyAccordionBody study={study} memberId={memberId} toggleOpen={toggleOpen} setModalInfo={setModalInfo} />
+        <StudyAccordionBody study={study} />
       </AccordionDetails>
     </Accordion>
   );
