001db26695fdf5e6f5132f978a681e2cf5ed1c18
jasper200207 <jasper12394@gmail.com>
2023-05-01 17:35:12 +0900

    design : 메인페이지 동아리 연혁 UI
    #123

---
+++ src/pages/home/History.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Typography } from '@material-tailwind/react';
+
+const dummyList = new Array(10).fill(0);
+
+const History = () => {
+  return (
+    <div className="w-full">
+      <div className="w-full pl-40">
+        {dummyList.map((v, index) => (
+          <div>
+            <div className="flex place-items-center">
+              <div className="h-7 w-7 rounded-full border-2 border-pointBlue" />
+              <Typography variant="h3" className="ml-10 font-bold text-pointBlue">
+                2022
+              </Typography>
+            </div>
+            <ul
+              className={`ml-3 list-disc border-pointBlue pb-16 pl-16${
+                index !== dummyList.length - 1 ? ' border-l-2' : ''
+              }`}
+            >
+              <li>
+                <Typography variant="h3">코드게이트 본선 진출</Typography>
+              </li>
+              <li>
+                <Typography variant="h3">코드게이트 본선 진출</Typography>
+              </li>
+              <li>
+                <Typography variant="h3">코드게이트 본선 진출</Typography>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+      <img alt="firework" src="/img/firework.png" className="w-full object-fill" />
+    </div>
+  );
+};
+
+export default History;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Intro from './Intro';
+import History from './History';
 
 const Home = () => {
   return (
     <div className="flex w-full flex-col bg-galaxy bg-contain bg-repeat">
       <Intro />
+      <History />
     </div>
   );
 };
