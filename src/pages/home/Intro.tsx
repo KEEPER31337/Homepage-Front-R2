8b2f8991a2b63bba5f472c206494a67c367fd067
pipisebastian <yrt7998@pusan.ac.kr>
2023-05-09 19:04:05 +0900

    Revert "feat : tab 폴더 명 대문자로 변경"
    This reverts commit 7e4ad5404cfde90834f6eac9d9c5a6bec741e24f.

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
-import StandardTab from '@components/Tab/StandardTab';
+import React from 'react';
+import { ReactComponent as LogoNeon } from '@assets/logo/logo_neon.svg';
+import { Typography } from '@material-tailwind/react';
 
 const Intro = () => {
-  const tabList = [
-    { id: 0, label: '도서 관리' },
-    { id: 1, label: '반납 관리' },
-    { id: 2, label: '흠냐 관리' },
-  ];
-  const [tab, setTab] = useState(0);
+  const introText =
+    "'지키다'라는 의미를 가진 단어 'keep'에서 착안하여,\n정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다.";
 
   return (
-    <div>
-      <StandardTab options={tabList} tab={tab} setTab={setTab} />
-
-      {tab === 0 && <div>도서</div>}
-      {tab === 1 && <div>반납</div>}
-      {tab === 2 && <div>흠냐</div>}
+    <div className="flex h-screen w-full flex-col place-content-center place-items-center">
+      <div className="space-y-24">
+        <LogoNeon className="w-[700px]" />
+        <Typography variant="h3" className="whitespace-pre-wrap text-center">
+          {introText}
+        </Typography>
+      </div>
     </div>
   );
 };
