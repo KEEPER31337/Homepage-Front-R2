68d0245f374e588edaf0726236dba2f7a731f59d
pipisebastian <yrt7998@pusan.ac.kr>
2023-05-15 00:33:26 +0900

    feat : outlineButton startIcon, endIcon 추가 가능하도록
    #359

--- src/components/Button/OutlinedButton.tsx
+++ src/components/Button/OutlinedButton.tsx
@@ -5,15 +5,19 @@ interface OutlinedButtonProps {
   children: ReactNode;
   onClick?: () => void;
   disabled?: boolean;
+  startIcon?: JSX.Element;
+  endIcon?: JSX.Element;
 }
 
-const OutlinedButton = ({ children, onClick, disabled }: OutlinedButtonProps) => {
+const OutlinedButton = ({ children, onClick, disabled, startIcon, endIcon }: OutlinedButtonProps) => {
   return (
     <Button
       variant="outlined"
       className="!focus:ring-0 !h-fit !rounded-sm !border-pointBlue !py-2 !px-6 !text-small !font-semibold !leading-4 !text-pointBlue"
       onClick={onClick}
       disabled={disabled}
+      startIcon={startIcon}
+      endIcon={endIcon}
     >
       {children}
     </Button>
