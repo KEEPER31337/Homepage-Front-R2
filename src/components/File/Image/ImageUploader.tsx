032a0c23b7ddbf24de729ab61bf8866197e2919f
amaran-th <songsy405@gmail.com>
2023-05-22 02:17:42 +0900

    feat: 주석 처리된 코드/esLint 문법에 맞지 않던 코드 수정
    #356

--- src/components/File/Image/ImageUploader.tsx
+++ src/components/File/Image/ImageUploader.tsx
@@ -3,6 +3,7 @@ import { useDropzone } from 'react-dropzone';
 import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
 
 import { StudyListInfo } from '@api/dto';
+import utilApi from '@mocks/UtilApi';
 
 interface ImageUploaderProps {
   title?: string;
@@ -13,68 +14,53 @@ interface ImageUploaderProps {
 
 const validateName = (fileName: string) => {
   const extensions = ['jpeg', 'jpg', 'png'];
-  const fparts = fileName.split('.');
-  let fext = '';
+  const fileParts = fileName.split('.');
+  let fileExtension = '';
 
-  if (fparts.length > 1) fext = fparts[fparts.length - 1];
+  if (fileParts.length > 1) fileExtension = fileParts[fileParts.length - 1];
   let validated = false;
-  if (fext !== '') {
+  if (fileExtension !== '') {
     extensions.forEach((ext) => {
-      if (ext === fext) validated = true;
+      if (ext === fileExtension) validated = true;
     });
   }
   return validated;
 };
 
 const ImageUploader = ({ title, isEdit, selectedStudy, setThumbnail }: ImageUploaderProps) => {
-  const [thumbnailBase64, setThumbnailBase64] = useState<string>(); // 파일 base64
-  // const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailBase64, setThumbnailBase64] = useState<string>();
 
   useEffect(() => {
-    // if (isEdit) {
-    //   const list = selectedStudy.thumbnailPath.split('/');
-    //   utilAPI.getThumbnail({ thumbnailId: list[list.length - 1] }).then((data: Blob) => {
-    //     setThumbnail(data);
-    //     const reader = new FileReader();
-    //     reader.onabort = () => {
-    //       /* console.log('file reading was aborted'); */
-    //     };
-    //     reader.onerror = () => {
-    //       /* console.log('file reading has failed'); */
-    //     };
-    //     reader.onloadend = () => {
-    //       const base64 = reader.result;
-    //       if (base64) {
-    //         setThumbnailBase64(base64.toString);
-    //       }
-    //     };
-    //     reader.readAsDataURL(data);
-    //   });
-    // }
+    if (isEdit && selectedStudy) {
+      const list = selectedStudy.thumbnailPath.split('/');
+      const data = utilApi.getThumbnail(list[list.length - 1]);
+      setThumbnail(data);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64 = reader.result;
+        if (base64) {
+          setThumbnailBase64(base64.toString);
+        }
+      };
+      reader.readAsDataURL(data);
+    }
   }, []);
 
   const deleteClickHandler = () => {
-    setThumbnailBase64(undefined);
+    setThumbnailBase64('');
     setThumbnail(new Blob());
   };
 
-  const onDrop = useCallback((acceptedFiles: any) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
     setThumbnailBase64('');
-    acceptedFiles.forEach((file: any) => {
+    acceptedFiles.forEach((file: File) => {
       if (validateName(file.name)) {
         setThumbnail(file);
         const reader = new FileReader();
-
-        reader.onabort = () => {
-          /* console.log('file reading was aborted'); */
-        };
-        reader.onerror = () => {
-          /* console.log('file reading has failed'); */
-        };
         reader.onloadend = () => {
           const array = [];
           for (let i = 0; i < (reader.result as string).length; i += 1) {
-            array.push(reader?.result?.toString().charCodeAt(i));
+            array.push((reader.result as string).charCodeAt(i));
           }
           const base64 = reader.result;
           if (base64) {
@@ -97,10 +83,14 @@ const ImageUploader = ({ title, isEdit, selectedStudy, setThumbnail }: ImageUplo
   };
 
   return (
-    <>
-      <div className="flex items-center justify-between">
+    <div className="w-full space-y-[10px]">
+      <div className="flex w-full items-center justify-between ">
         <span className="text-paragraph">{title || '썸네일'}</span>
-        <button className="text-small underline underline-offset-4" type="button" onClick={deleteClickHandler}>
+        <button
+          className="text-small underline underline-offset-4 hover:text-pointBlue"
+          type="button"
+          onClick={deleteClickHandler}
+        >
           기본 이미지로
         </button>
       </div>
@@ -109,7 +99,7 @@ const ImageUploader = ({ title, isEdit, selectedStudy, setThumbnail }: ImageUplo
         {...rootProps}
         className={`${isDragActive ? 'bg-pointBlue/[30%] bg-opacity-50' : ''} ${
           thumbnailBase64 ? '' : 'border-[2px]'
-        } flex h-32 w-32 items-center justify-center border-dashed !border-pointBlue/[30%]`}
+        } flex h-32 w-full items-center justify-center border-dashed !border-pointBlue/[30%]`}
       >
         {thumbnailBase64 ? (
           <img
@@ -134,7 +124,7 @@ const ImageUploader = ({ title, isEdit, selectedStudy, setThumbnail }: ImageUplo
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 export default ImageUploader;
