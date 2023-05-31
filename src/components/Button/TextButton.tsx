2c6672d6da80c856237c56d1c853ada030f68cc8
publdaze <publdaze@naver.com>
2023-05-31 18:02:41 +0900

    feat : 버튼에 type prop으로 받아오도록 확장
    #377

--- src/components/Button/TextButton.tsx
+++ src/components/Button/TextButton.tsx
@@ -5,12 +5,14 @@ interface TextButtonProps {
   children: ReactNode;
   onClick?: () => void;
   disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const TextButton = ({ children, onClick, disabled }: TextButtonProps) => {
+const TextButton = ({ children, onClick, disabled, type }: TextButtonProps) => {
   return (
     <Button
       variant="text"
+      type={type}
       className="h-fit rounded-sm py-2 font-base text-pointBlue hover:bg-pointBlue/10 active:bg-pointBlue/30"
       onClick={onClick}
       disabled={disabled}
