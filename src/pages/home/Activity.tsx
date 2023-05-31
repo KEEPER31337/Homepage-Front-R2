6365a72ea2e415eea12c895dcdf40696d571c4b9
jasper200207 <jasper12394@gmail.com>
2023-06-01 00:46:49 +0900

    refactor : subTitleImage 컴포넌트로 분리
    - SubTitleImageCard 컴포넌트를 만들어 정기활동 내용을 분리
    #121

--- src/pages/home/Activity.tsx
+++ src/pages/home/Activity.tsx
@@ -1,5 +1,17 @@
 import { useGetBlockListQuery } from '@api/aboutApi';
 import React from 'react';
+import { SubTitleImagesInfo } from '@api/dto';
+
+const SubTitleImageCard = ({ subtitleImage }: { subtitleImage: SubTitleImagesInfo }) => {
+  return (
+    <div className="w-full whitespace-pre-line px-10 leading-10">
+      <p className="mb-8 text-2xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
+      <p className="bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">
+        {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n`)}
+      </p>
+    </div>
+  );
+};
 
 const Activity = () => {
   const { data } = useGetBlockListQuery({ type: 'activity' });
@@ -8,24 +20,14 @@ const Activity = () => {
     <div className="bg-mainBlack">
       {data?.subtitleImages?.map((subtitleImage, index) =>
         index % 2 === 0 ? (
-          <div key={subtitleImage.id} className="flex w-full flex-row place-items-center">
-            <img src="/img/image3.png" alt="" className="w-[40%]" />
-            <div className="flex w-[60%] flex-col whitespace-pre-line px-10 text-left leading-10">
-              <p className="mb-8 text-2xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
-              <p className="bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">
-                {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n`)}
-              </p>
-            </div>
+          <div className="grid grid-cols-[40%_60%] place-items-center text-left">
+            <img src={subtitleImage.thumbnailPath || '/img/image3.png'} alt="" />
+            <SubTitleImageCard subtitleImage={subtitleImage} />
           </div>
         ) : (
-          <div key={subtitleImage.id} className="flex w-full flex-row place-items-center">
-            <div className="flex w-[40%] flex-col whitespace-pre-line px-10 text-left leading-10">
-              <p className="mb-8 text-2xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
-              <p className="bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">
-                {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n`)}
-              </p>
-            </div>
-            <img src="/img/image2.png" alt="" className="w-[60%]" />
+          <div className="grid grid-cols-[40%_60%] place-items-center text-right">
+            <SubTitleImageCard subtitleImage={subtitleImage} />
+            <img src={subtitleImage.thumbnailPath || '/img/image2.png'} alt="" />
           </div>
         ),
       )}
