a06e4c9e266bd82318e38cf8e419885beec2a926
lcqff <71930280+lcqff@users.noreply.github.com>
2023-06-10 16:29:09 +0900

    refactor: seminarCard className 제거
    #331

--- src/pages/senimarAttend/Card/SeminarCard.tsx
+++ src/pages/senimarAttend/Card/SeminarCard.tsx
@@ -3,17 +3,16 @@ import { Card, CardContent } from '@mui/material';
 
 interface SeminarCardProps {
   children: ReactNode;
-  className?: string;
 }
 
-const SeminarCard = ({ children, className }: SeminarCardProps) => {
+const SeminarCard = ({ children }: SeminarCardProps) => {
   const seminarExist = false; // TODO: useState, api로 교체
 
   return (
     <Card
       className={`flex h-[426px] w-96 items-center justify-center !bg-middleBlack !bg-none ${
         seminarExist && 'opacity-50'
-      } ${className}`}
+      } `}
     >
       <CardContent className="h-[315px]">{children}</CardContent>
     </Card>
