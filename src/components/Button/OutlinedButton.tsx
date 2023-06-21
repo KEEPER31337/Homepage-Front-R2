2a9a56b9c7ad7a4dcc6be93891ee9be5c2f19384
pipisebastian <yrt7998@pusan.ac.kr>
2023-06-21 17:52:38 +0900

    design : disabled시 border색 pointColor 제거
    #397

--- src/components/Button/OutlinedButton.tsx
+++ src/components/Button/OutlinedButton.tsx
@@ -15,7 +15,9 @@ const OutlinedButton = ({ children, onClick, disabled, type, small, startIcon, e
   return (
     <Button
       variant="outlined"
-      className={`${small && '!text-small'} h-fit !rounded-sm !border-pointBlue !py-2 !px-6 !font-semibold`}
+      className={`${small && '!text-small'} ${
+        !disabled && '!border-pointBlue'
+      } h-fit !rounded-sm !py-2 !px-6 !font-semibold`}
       type={type}
       onClick={onClick}
       disabled={disabled}
