563cf0b0ed525b33047c967a4e31169c732bf06e
publdaze <publdaze@naver.com>
2023-06-25 11:44:08 +0900

    feat : 카테고리 버튼 링크로 해당 경로 이동 되도록 처리
    #271

--- src/components/Navigation/SubCategoryNav.tsx
+++ src/components/Navigation/SubCategoryNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CategoryMenu } from '@constants/category';
 import { ListItemButton, ListItemText } from '@mui/material';
 
@@ -8,7 +9,7 @@ interface SubCategoryNavProps {
 
 const SubCategoryNav = ({ subcategory }: SubCategoryNavProps) => {
   return (
-    <ListItemButton className="w-full">
+    <ListItemButton component={Link} to={`/${subcategory.path}`} className="w-full">
       <ListItemText primary={`• ${subcategory.name}`} />
     </ListItemButton>
   );
