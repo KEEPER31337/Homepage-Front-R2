58f4bd968522957ab9b3336be60d94f070602fca
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2023-07-01 02:16:00 +0900

    refactor : 피드백 반영
    - SubTitleImageCard 리펙토링
    - 기본이미지 로고 이미지로 변경
    - 인포 사이에 엔터 추가
    - #121

--- src/pages/home/Activity.tsx
+++ src/pages/home/Activity.tsx
@@ -1,14 +1,24 @@
 import { useGetBlockListQuery } from '@api/aboutApi';
 import React from 'react';
+import LogoNeon from '@assets/logo/logo_neon.svg';
 import { SubTitleImagesInfo } from '@api/dto';
 
 const SubTitleImageCard = ({ subtitleImage }: { subtitleImage: SubTitleImagesInfo }) => {
   return (
-    <div className="w-full whitespace-pre-line px-10 leading-10">
-      <p className="mb-8 text-3xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
-      <p className="bg-gradient-to-b from-gray-500 to-white bg-clip-text text-xl text-transparent">
-        {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n`)}
-      </p>
+    <div className="gird-cols-[45%_55%] group grid grid-flow-col place-items-center text-left even:text-right">
+      <img
+        className={`h-[950px] group-even:order-last ${
+          subtitleImage?.thumbnailPath ? 'w-full object-cover' : 'w-96 object-contain'
+        }`}
+        src={subtitleImage?.thumbnailPath || LogoNeon}
+        alt=""
+      />
+      <div className="w-full whitespace-pre-line px-10 leading-10">
+        <p className="mb-8 text-3xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
+        <p className="bg-gradient-to-b from-gray-500 to-white bg-clip-text text-xl text-transparent">
+          {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n\n`)}
+        </p>
+      </div>
     </div>
   );
 };
@@ -18,19 +28,9 @@ const Activity = () => {
 
   return (
     <div className="bg-mainBlack">
-      {data?.subtitleImages?.map((subtitleImage, index) =>
-        index % 2 === 0 ? (
-          <div className="grid grid-cols-[40%_60%] place-items-center text-left">
-            <img src={subtitleImage.thumbnailPath || '/img/image3.png'} alt="" />
-            <SubTitleImageCard subtitleImage={subtitleImage} />
-          </div>
-        ) : (
-          <div className="grid grid-cols-[40%_60%] place-items-center text-right">
-            <SubTitleImageCard subtitleImage={subtitleImage} />
-            <img src={subtitleImage.thumbnailPath || '/img/image2.png'} alt="" />
-          </div>
-        ),
-      )}
+      {data?.subtitleImages?.map((subtitleImage) => (
+        <SubTitleImageCard key={subtitleImage.id} subtitleImage={subtitleImage} />
+      ))}
     </div>
   );
 };
