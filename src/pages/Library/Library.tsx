8b9da636344409b54c73e46d5eec242915507368
pipisebastian <yrt7998@pusan.ac.kr>
2023-06-30 16:53:29 +0900

    design : 도서 검색 ui 구현
    #369

---
+++ src/pages/Library/Library.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PageTitle from '@components/Typography/PageTitle';
+import SearchSection from '@components/Section/SearchSection';
+import StandardTablePagination from '@components/Pagination/StandardTablePagination';
+import BookCard from './Card/BookCard';
+
+const Library = () => {
+  return (
+    <div>
+      <PageTitle>도서검색</PageTitle>
+      <div className="mb-5 flex w-full items-center justify-between">
+        <SearchSection />
+        <div className="mr-6 text-pointBlue">신청 가능 권수 : 1/5</div>
+      </div>
+      <div className="grid h-[408px] grid-cols-2 content-start gap-1 bg-middleBlack">
+        <BookCard />
+        <BookCard />
+        <BookCard />
+      </div>
+      <StandardTablePagination />
+    </div>
+  );
+};
+
+export default Library;
