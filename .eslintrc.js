cb464f1e4e77c389b46a162bd14c490c88b5eae8
lcqff <yeonnu82g@gmail.com>
2023-07-02 00:18:51 +0900

    feat: eslint import 순서 정렬 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   extends: [
     'airbnb',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   rules: {
     'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
@@ -26,6 +28,23 @@ module.exports = {
       },
     ],
     'import/no-extraneous-dependencies': ['error', { devDependencies: ['craco.config.ts', 'prettier.config.js'] }],
+    'import/order': [
+      'error',
+      {
+        groups: ['external', 'internal', 'index', 'sibling', 'parent', 'builtin', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
   },
   settings: {
     'import/resolver': {
