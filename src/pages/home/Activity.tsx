15901acaf4fdbc35923f98e0f02358112fd398c1
jasper200207 <jasper12394@gmail.com>
2023-07-02 00:32:01 +0900

    design : 좌측정렬로 통일
    - #121

--- src/pages/home/Activity.tsx
+++ src/pages/home/Activity.tsx
@@ -5,17 +5,17 @@ import { SubTitleImagesInfo } from '@api/dto';
 
 const SubTitleImageCard = ({ subtitleImage }: { subtitleImage: SubTitleImagesInfo }) => {
   return (
-    <div className="gird-cols-[45%_55%] group grid grid-flow-col place-items-center text-left even:text-right">
-      <img
-        className={`h-[950px] group-even:order-last ${
-          subtitleImage?.thumbnailPath ? 'w-full object-cover' : 'w-96 object-contain'
-        }`}
-        src={subtitleImage?.thumbnailPath || LogoNeon}
-        alt=""
-      />
-      <div className="w-full whitespace-pre-line px-10 leading-10">
+    <div className="group grid h-[960px] grid-cols-[45%_55%]">
+      <div className="flex place-content-center place-items-center group-even:order-last">
+        <img
+          className={subtitleImage?.thumbnailPath ? 'w-full object-cover' : 'w-96 object-contain'}
+          src={subtitleImage?.thumbnailPath || LogoNeon}
+          alt={subtitleImage?.subtitle}
+        />
+      </div>
+      <div className="flex flex-col place-content-center pl-14 text-left">
         <p className="mb-8 text-3xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
-        <p className="bg-gradient-to-b from-gray-500 to-white bg-clip-text text-xl text-transparent">
+        <p className="whitespace-pre-line bg-gradient-to-b from-gray-500 to-white bg-clip-text text-xl text-transparent">
           {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n\n`)}
         </p>
       </div>
