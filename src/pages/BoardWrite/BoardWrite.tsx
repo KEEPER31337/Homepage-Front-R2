101e1b2965a3118e8c7b9f12f3620a7e15ad1488
publdaze <publdaze@naver.com>
2023-07-15 22:24:52 +0900

    feat: 게시글 생성 페이지 - 에디터, 파일첨부, 버튼 섹션 구성
    #429

--- src/pages/BoardWrite/BoardWrite.tsx
+++ src/pages/BoardWrite/BoardWrite.tsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import { Checkbox, FormControlLabel, FormGroup, Stack, Typography } from '@mui/material';
+import React, { useRef } from 'react';
+import { Stack, Typography } from '@mui/material';
+import { Editor } from '@toast-ui/react-editor';
 import PageTitle from '@components/Typography/PageTitle';
 import StandardInput from '@components/Input/StandardInput';
+import StandardEditor from '@components/Editor/StandardEditor';
+import FileUploader from '@components/Uploader/FileUploader';
+import OutlinedButton from '@components/Button/OutlinedButton';
 
 const BoardWrite = () => {
   const boardName = '자유게시판'; // TODO 게시판 이름 불러오기
+  const editorRef = useRef<Editor>();
+
   return (
     <div>
       <PageTitle>{boardName}</PageTitle>
       <div className="mb-5 flex w-full items-center">
         <Stack flexDirection="row" marginRight={4} alignItems="center">
-          <Typography typography="semibold" className="!mr-2">
+          <Typography fontWeight="semibold" className="!mr-2">
             제목
           </Typography>
           <StandardInput
-            className="w-96"
+            className="w-[498px]"
             value=""
             onChange={() => {
               // TODO
             }}
           />
         </Stack>
-        <FormGroup className="flex !flex-row">
-          <FormControlLabel control={<Checkbox />} label="공지" />
-          <FormControlLabel control={<Checkbox />} label="비밀글" />
-        </FormGroup>
-        {/* TODO 비밀글 체크 시에만 렌더링 되도록 처리 */}
-        <StandardInput
-          className="w-32"
-          value=""
-          type="password"
-          placeholder="게시글 비밀번호"
-          onChange={() => {
-            // TODO
-          }}
-        />
+      </div>
+      <StandardEditor height="470px" forwardedRef={editorRef as React.MutableRefObject<Editor>} />
+      <Typography fontWeight="semibold" className="!mb-2 !mt-5">
+        파일첨부
+      </Typography>
+      <div className="mb-5 h-32">
+        <FileUploader />
+      </div>
+      <div className="flex justify-end space-x-2">
+        <OutlinedButton>임시저장</OutlinedButton>
+        <OutlinedButton>작성완료</OutlinedButton>
       </div>
     </div>
   );
