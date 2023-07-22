744fdf231497938f8fdd4ffbc6f54e84374b4ff4
jasper200207 <jasper12394@gmail.com>
2023-07-22 21:28:32 +0900

    feat : api ui 반영
    - HistoryItme으로 컴포넌트를 만들어 분리
    - #123

--- src/pages/home/History.tsx
+++ src/pages/home/History.tsx
@@ -1,36 +1,36 @@
-import { Typography } from '@mui/material';
+import { useGetBlockListQuery } from '@api/aboutApi';
 import React from 'react';
+import { SubTitleImagesInfo } from '@api/dto';
 
-const dummyList = new Array(10).fill(0);
+const HistoryItem = ({ subtitleImage }: { subtitleImage: SubTitleImagesInfo }) => {
+  return (
+    <>
+      <div className="flex place-items-center">
+        <div className="relative h-7 w-7 rounded-full border-2 border-pointBlue" />
+        <p className="ml-8 text-xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
+      </div>
+      <ul className="ml-[13px] list-disc border-l-2 border-pointBlue pb-16 pl-16 pt-3 last:border-0">
+        {subtitleImage?.staticWriteContents?.map((staticWriteContent) => (
+          <li key={staticWriteContent.id}>
+            <p>{staticWriteContent.content}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 const History = () => {
+  const { data: historyList } = useGetBlockListQuery({ type: 'history' });
+  const subtitleImages = historyList?.subtitleImages
+    ?.filter((subtitleImage) => subtitleImage?.staticWriteContents?.length)
+    .reverse();
+
   return (
     <div className="w-full">
       <div className="w-full bg-gradient-to-b from-middleBlack via-middleBlack to-90% pl-40 pt-60">
-        {dummyList.map((v, index) => (
-          <div>
-            <div className="flex place-items-center">
-              <div className="h-7 w-7 rounded-full border-2 border-pointBlue" />
-              <Typography variant="h3" className="ml-10 font-bold text-pointBlue">
-                2022
-              </Typography>
-            </div>
-            <ul
-              className={`ml-3 list-disc border-pointBlue pb-16 pl-16${
-                index !== dummyList.length - 1 ? ' border-l-2' : ''
-              }`}
-            >
-              <li>
-                <Typography variant="h3">코드게이트 본선 진출</Typography>
-              </li>
-              <li>
-                <Typography variant="h3">코드게이트 본선 진출</Typography>
-              </li>
-              <li>
-                <Typography variant="h3">코드게이트 본선 진출</Typography>
-              </li>
-            </ul>
-          </div>
+        {subtitleImages?.map((subtitleImage) => (
+          <HistoryItem subtitleImage={subtitleImage} key={subtitleImage.id} />
         ))}
       </div>
       <img alt="firework" src="/img/firework.png" className="w-full object-fill" />
