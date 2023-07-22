091cb4e6c6538de1a7a2036dca1b22d329b29e8d
jasper200207 <jasper12394@gmail.com>
2023-07-22 20:10:31 +0900

    feat : api ui에 반영
    - 불러온 api 내용 ui 반영
    - #122

--- src/pages/home/Excellence.tsx
+++ src/pages/home/Excellence.tsx
@@ -7,22 +7,20 @@ const demoContents = '도서 및 기자재 지원\n스터디룸 비용 지원\n�
 const Excellence = () => {
   const { data: excellenceList } = useGetBlockListQuery({ type: 'excellence' });
 
+  console.log(excellenceList);
+
   return (
     <div className="flex w-full flex-row bg-mainBlack">
       <img src="/img/main/excellence.png" alt="" className="w-[60%]" />
       <div className="flex w-full flex-col place-content-center px-10">
-        <div className="w-full whitespace-pre-line border-b-2 border-pointBlue py-8 text-left leading-7">
-          <p className="ml-5 mb-8 text-2xl font-bold text-pointBlue">{demoTitle}</p>
-          <p className="ml-10 bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">{demoContents}</p>
-        </div>
-        <div className="w-full whitespace-pre-line border-b-2 border-pointBlue py-8 text-left leading-7">
-          <p className="ml-5 mb-8 text-2xl font-bold text-pointBlue">{demoTitle}</p>
-          <p className="ml-10 bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">{demoContents}</p>
-        </div>
-        <div className="w-full whitespace-pre-line py-8 text-left leading-7">
-          <p className="ml-5 mb-8 text-2xl font-bold text-pointBlue">{demoTitle}</p>
-          <p className="ml-10 bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">{demoContents}</p>
-        </div>
+        {excellenceList?.subtitleImages?.map((subtitleImage) => (
+          <div className="w-full whitespace-pre-line border-b-2 border-pointBlue py-8 text-left leading-7 last:border-0">
+            <p className="mb-5 ml-5 text-2xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
+            <p className="ml-10 bg-gradient-to-b from-gray-500 to-white bg-clip-text text-transparent">
+              {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n`)}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
