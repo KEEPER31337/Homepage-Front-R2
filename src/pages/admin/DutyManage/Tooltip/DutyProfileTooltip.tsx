3270f44238efe14cb137e1832ab822dd4f5aaae1
llddang <bodyness@naver.com>
2023-07-25 01:00:10 -0400

    feat: 직책관리 페이지 전산관리자 UI 변경 반영
    - Feature/직책관리 페이지 UI 구성 #447 코드리뷰 반영
    - 직책관리 페이지 전산관리자 UI 변경 반영
    #447

---
+++ src/pages/admin/DutyManage/Tooltip/DutyProfileTooltip.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useReducer } from 'react';
+import { Typography } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { roleDutyListInfo, roles } from '@mocks/DutyManageApi';
+
+import DescriptionRoleDutyTooltip from './DescriptionRoleDutyTooltip';
+import DutyProfileButton from '../Button/DutyProfileButton';
+import ChangeRolePersonModal from '../Modal/ChangeRolePersonModal';
+
+interface DutyProfileTooltipProps {
+  roleName: string;
+}
+
+const DutyProfileTooltip = ({ roleName }: DutyProfileTooltipProps) => {
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const [modalOpen, toggleModalOpen] = useReducer((prev) => !prev, false);
+
+  const badgeImage = roles.filter((role) => role.name === roleName)[0].img;
+  const modalInfo = { name: roleName, badge: badgeImage };
+
+  /* button hover 했을 시 나오는 문장 */
+  const tooltipContent = (
+    <List sx={{ px: '8px', py: '16px' }}>
+      {roleDutyListInfo
+        .filter((duty) => duty.roleName === roleName)
+        .map((data) =>
+          data.roleDuty.map((duty) => (
+            <ListItem key={duty.key} sx={{ px: '0px', py: '2px', alignItems: 'start' }}>
+              <ListItemIcon
+                sx={{
+                  height: '24px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  minWidth: '0px',
+                  paddingRight: '8px',
+                  alignContent: 'flex-start',
+                }}
+              >
+                {/* 문장의 제일 앞에 나오는 원 icon */}
+                <div className="h-2 w-2 rounded-full border-2 border-pointBlue" />
+              </ListItemIcon>
+              {/* fontWeight: 600 = semi-bold */}
+              <Typography sx={{ fontWeight: 600, color: 'white' }}>{duty.content}</Typography>
+            </ListItem>
+          )),
+        )}
+    </List>
+  );
+
+  return (
+    <DescriptionRoleDutyTooltip
+      title={tooltipContent}
+      open={tooltipOpen}
+      onMouseEnter={() => setTooltipOpen(true)}
+      onMouseLeave={() => setTooltipOpen(false)}
+      arrow
+    >
+      <div>
+        <DutyProfileButton roleName={roleName} setTooltipOpen={setTooltipOpen} toggleModalOpen={toggleModalOpen} />
+        <ChangeRolePersonModal open={modalOpen} toggleOpen={toggleModalOpen} modalInfo={modalInfo} />
+      </div>
+    </DescriptionRoleDutyTooltip>
+  );
+};
+
+export default DutyProfileTooltip;
