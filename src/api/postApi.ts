bc265638cf0a244753a10fc33182b21e36dd0ff8
publdaze <publdaze@naver.com>
2023-08-01 11:23:41 +0900

    feat : 게시글 생성 API 작성
    #431

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -3,7 +3,16 @@ import { useMutation } from 'react-query';
 import { UploadPost } from './dto';
 
 const useUploadPostMutation = () => {
-  const fetcher = (postInfo: UploadPost) => axios.post('/posts', postInfo);
+  const fetcher = (postInfo: UploadPost) => {
+    const formData = new FormData();
+    formData.append('request', new Blob([JSON.stringify(postInfo)], { type: 'application/json' }));
+
+    return axios.post('/posts', formData, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    });
+  };
 
   return useMutation(fetcher);
 };
