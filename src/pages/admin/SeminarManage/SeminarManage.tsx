2c908ffccd2ba71d981f4d6ac4a6425565b1cb83
publdaze <publdaze@naver.com>
2023-08-16 13:07:56 +0900

    feat : 추가/삭제 버튼 배치 조정 및 공통 컴포넌트 사용
    #139

--- src/pages/admin/SeminarManage/SeminarManage.tsx
+++ src/pages/admin/SeminarManage/SeminarManage.tsx
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import StandardTable from '@components/Table/StandardTable';
 import { columns, rows } from '@mocks/SeminarManageApi';
 import PageTitle from '@components/Typography/PageTitle';
-import OutlinedButton from '@components/Button/OutlinedButton';
 
+import ActionButton from '@components/Button/ActionButton';
 import AddSeminarModal from './Modal/AddSeminarModal';
 
 const SeminarManage = () => {
   const listColumns = columns;
   const listRows = rows;
   const currentTerm = { year: 2023, season: '1학기' }; // TODO - 임시데이터
-  const [openSeminarAddModal, toggleSeminarAddModal] = useState(false); // useReducer((prev) => !prev, false);
+  const [openSeminarAddModal, toggleSeminarAddModal] = useState(false);
 
   return (
     <div>
       <PageTitle>세미나 관리 {`(${currentTerm.year}년 ${currentTerm.season})`}</PageTitle>
-      <StandardTable columns={listColumns} rows={listRows} />
-      <div className="mt-4 flex justify-end space-x-2">
-        <OutlinedButton onClick={() => toggleSeminarAddModal(true)}>추가</OutlinedButton>
-        <OutlinedButton>삭제</OutlinedButton>
+      <div className="mb-5 flex justify-end space-x-2">
+        <ActionButton mode="add" onClick={() => toggleSeminarAddModal(true)}>
+          추가
+        </ActionButton>
+        <ActionButton mode="delete">삭제</ActionButton>
       </div>
+      <StandardTable columns={listColumns} rows={listRows} />
       <AddSeminarModal open={openSeminarAddModal} toggleOpen={toggleSeminarAddModal} />
     </div>
   );
