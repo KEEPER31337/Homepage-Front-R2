6d6af69c1894a634d5152224388cd5412ad8ff7f
publdaze <publdaze@naver.com>
2023-08-21 01:00:38 +0900

    feat : header 로그인에 따른 처리
    #530

--- src/components/Layout/Header/Menu/AccountMenu.tsx
+++ src/components/Layout/Header/Menu/AccountMenu.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Menu, MenuItem } from '@mui/material';
+import { Divider, Menu, MenuItem, MenuList } from '@mui/material';
+import { MemberInfo } from '@api/dto';
 
 interface AccountMenuProps {
+  userInfo: MemberInfo;
   anchorEl: HTMLElement | null;
   open: boolean;
   onClose: () => void;
 }
 
-const AccountMenu = ({ anchorEl, open, onClose }: AccountMenuProps) => {
+const AccountMenu = ({ userInfo, anchorEl, open, onClose }: AccountMenuProps) => {
   const accountMenuList = [
     { id: 1, content: '프로필' },
     { id: 2, content: '로그아웃' },
@@ -15,6 +17,8 @@ const AccountMenu = ({ anchorEl, open, onClose }: AccountMenuProps) => {
 
   return (
     <Menu anchorEl={anchorEl} open={open} onClose={onClose}>
+      <MenuList className="text-center">{userInfo.emailAddress}</MenuList>
+      <Divider className="!my-1" />
       {accountMenuList.map((accountMenu) => (
         <MenuItem className="min-w-[150px]" key={accountMenu.id}>
           {accountMenu.content}
