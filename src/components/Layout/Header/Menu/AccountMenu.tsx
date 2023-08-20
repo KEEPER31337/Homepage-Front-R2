db1c9d22158457b4dc526983efdba8aa5cc26491
publdaze <publdaze@naver.com>
2023-08-21 01:12:05 +0900

    feat : 로그아웃 기능 구현
    #530

--- src/components/Layout/Header/Menu/AccountMenu.tsx
+++ src/components/Layout/Header/Menu/AccountMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Divider, Menu, MenuItem, MenuList } from '@mui/material';
 import { MemberInfo } from '@api/dto';
+import { useSignOutMutation } from '@api/authApi';
 
 interface AccountMenuProps {
   userInfo: MemberInfo;
@@ -10,20 +11,20 @@ interface AccountMenuProps {
 }
 
 const AccountMenu = ({ userInfo, anchorEl, open, onClose }: AccountMenuProps) => {
-  const accountMenuList = [
-    { id: 1, content: '프로필' },
-    { id: 2, content: '로그아웃' },
-  ];
+  const { mutate: logout } = useSignOutMutation();
+
+  const handleLogOutClick = () => {
+    logout();
+  };
 
   return (
     <Menu anchorEl={anchorEl} open={open} onClose={onClose}>
       <MenuList className="text-center">{userInfo.emailAddress}</MenuList>
       <Divider className="!my-1" />
-      {accountMenuList.map((accountMenu) => (
-        <MenuItem className="min-w-[150px]" key={accountMenu.id}>
-          {accountMenu.content}
-        </MenuItem>
-      ))}
+      <MenuItem className="min-w-[150px]">프로필</MenuItem>
+      <MenuItem className="min-w-[150px]" onClick={handleLogOutClick}>
+        로그아웃
+      </MenuItem>
     </Menu>
   );
 };
