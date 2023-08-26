dd02f9f7c0236ecbcb0202c7dd2acc8c2ba3d3f3
lcqff <yeonnu82g@gmail.com>
2023-08-26 15:09:34 +0900

    feat: 권한에 따른 페이지 이동

--- src/pages/senimarAttend/SenimarAttend.tsx
+++ src/pages/senimarAttend/SenimarAttend.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import FilledButton from '@components/Button/FilledButton';
+import React, { useEffect } from 'react';
 import { Typography } from '@mui/material';
-import {
-  useGetRecentlyDoneSeminarInfoQuery,
-  useGetRecentlyUpcomingSeminarInfoQuery,
-  useGetSeminarInfoQuery,
-} from '@api/seminarApi';
+import { useGetRecentlyDoneSeminarInfoQuery, useGetRecentlyUpcomingSeminarInfoQuery } from '@api/seminarApi';
+import useCheckAuth from '@hooks/useCheckAuth';
 import SeminarCard from './Card/SeminarCard';
 import BossCardContent from './Card/BossCardContent';
 import MemberCardContent from './Card/MemberCardContent';
@@ -16,37 +12,31 @@ const SeminarAttend = () => {
   const recentSeminarId = twoUpcomingSeminarIds && twoUpcomingSeminarIds[0]?.id;
   const futureSeminarId = twoUpcomingSeminarIds && twoUpcomingSeminarIds[1]?.id;
   const cardIdOrder = [recentlyDoneSeminarId, recentSeminarId, futureSeminarId];
-  const [isBoss, setIsBoss] = useState(true); // TODO: api? 적용?
+  const { checkIncludeOneOfAuths } = useCheckAuth();
+  const isBoss = checkIncludeOneOfAuths(['ROLE_회장', 'ROLE_부회장', 'ROLE_서기']);
 
   useEffect(() => {
     if (!localStorage.getItem('출석시도횟수')) localStorage.setItem('출석시도횟수', '0');
   }, []);
 
   return (
-    <>
-      <div className="mt-[180px] flex justify-between text-center [&>*:nth-child(2)]:mt-[-50px]">
-        {cardIdOrder.map((seminarId) => {
-          return (
-            <SeminarCard key={seminarId}>
-              {seminarId !== undefined ? (
-                <div>
-                  {isBoss ? <BossCardContent seminarId={seminarId} /> : <MemberCardContent seminarId={seminarId} />}
-                </div>
-              ) : (
-                <Typography className="!mt-[16px] text-center !text-h3 !font-bold text-pointBlue opacity-50">
-                  예정된 세미나가 없습니다.
-                </Typography>
-              )}
-            </SeminarCard>
-          );
-        })}
-      </div>
-      {isBoss ? (
-        <FilledButton onClick={() => setIsBoss(false)}>회원권한페이지로</FilledButton>
-      ) : (
-        <FilledButton onClick={() => setIsBoss(true)}>회장권한페이지로</FilledButton>
-      )}
-    </>
+    <div className="mt-[180px] flex justify-between text-center [&>*:nth-child(2)]:mt-[-50px]">
+      {cardIdOrder.map((seminarId) => {
+        return (
+          <SeminarCard key={seminarId}>
+            {seminarId !== undefined ? (
+              <div>
+                {isBoss ? <BossCardContent seminarId={seminarId} /> : <MemberCardContent seminarId={seminarId} />}
+              </div>
+            ) : (
+              <Typography className="!mt-[16px] text-center !text-h3 !font-bold text-pointBlue opacity-50">
+                예정된 세미나가 없습니다.
+              </Typography>
+            )}
+          </SeminarCard>
+        );
+      })}
+    </div>
   );
 };
 
