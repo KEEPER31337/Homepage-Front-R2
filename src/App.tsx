fe1e8d674ee4861e32bc665665d117c11b415e6c
publdaze <publdaze@naver.com>
2023-08-26 10:46:36 +0900

    feat : 훅 내부에서 useNavigate 사용을 위해 global 에러처리 Router 내부로 이동
    #502

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import useMainRouter from '@router/useMainRouter';
+import { useApiError } from '@hooks/useGetApiError';
+import { useQueryClient } from 'react-query';
 
 const App = () => {
+  const { handleError } = useApiError();
+  const queryClient = useQueryClient();
+
+  queryClient.setDefaultOptions({
+    queries: {
+      onError: handleError,
+    },
+    mutations: {
+      onError: handleError,
+    },
+  });
+
   const routes = useMainRouter();
 
   return <div className="bg-subBlack">{routes}</div>;
