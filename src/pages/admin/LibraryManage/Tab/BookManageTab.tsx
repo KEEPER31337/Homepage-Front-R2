c889b506983eb3bdfe97ce10e7218a989cef39b9
pipisebastian <yrt7998@pusan.ac.kr>
2023-08-26 18:15:39 +0900

    feat : search section추가
    #548

--- src/pages/admin/LibraryManage/Tab/BookManageTab.tsx
+++ src/pages/admin/LibraryManage/Tab/BookManageTab.tsx
@@ -5,11 +5,23 @@ import StandardTable from '@components/Table/StandardTable';
 import OutlinedButton from '@components/Button/OutlinedButton';
 import { ChildComponent } from '@components/Table/StandardTable.interface';
 import { columns, rows } from '@mocks/LibraryManageApi';
-
+import SearchSection from '@components/Section/SearchSection';
 import AddBookModal from '../Modal/AddBookModal';
 import DeleteBookModal from '../Modal/DeleteBookModal';
 
 const BookManageTab = () => {
+  const bookNumberList = [
+    { id: 'all', content: '도서명 + 저자' },
+    { id: 'title', content: '도서명' },
+    { id: 'author', content: '저자' },
+  ];
+  const [inputValue, setInputValue] = useState('');
+  const [selectorValue, setSelectorValue] = useState('all');
+
+  const handleSearchButtonClick = () => {
+    console.log(selectorValue, inputValue, '검색 api 호출');
+  };
+
   const childComponent = ({ key, value }: ChildComponent<BookListInfo>) => {
     switch (key) {
       case 'enable':
@@ -23,13 +35,20 @@ const BookManageTab = () => {
 
   return (
     <>
-      <div className="flex space-x-2">
+      <div className="flex justify-between space-x-2">
+        <SearchSection
+          options={bookNumberList}
+          selectorValue={selectorValue}
+          setSelectorValue={setSelectorValue}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          onSearchButtonClick={handleSearchButtonClick}
+        />
         <OutlinedButton onClick={() => setAddBookModalOpen(true)}>도서 추가</OutlinedButton>
-        <OutlinedButton onClick={() => setDeleteBookModalOpen(true)}>도서 삭제</OutlinedButton>
-
         <AddBookModal open={addBookModalOpen} onClose={() => setAddBookModalOpen(false)} />
-        <DeleteBookModal open={deleteBookModalOpen} onClose={() => setDeleteBookModalOpen(false)} />
       </div>
+      {/* <OutlinedButton onClick={() => setDeleteBookModalOpen(true)}>도서 삭제</OutlinedButton>
+      <DeleteBookModal open={deleteBookModalOpen} onClose={() => setDeleteBookModalOpen(false)} /> */}
       <StandardTable<BookListInfo> columns={columns} rows={rows} childComponent={childComponent} />
     </>
   );
