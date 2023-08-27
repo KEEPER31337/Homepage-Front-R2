d16fcaeff74b1a529fe55aaa649cce57df7838d7
lcqff <yeonnu82g@gmail.com>
2023-08-28 04:17:26 +0900

    feat: ReactCodesInput로 라이브러리 변경

--- src/pages/senimarAttend/Input/SeminarInput.tsx
+++ src/pages/senimarAttend/Input/SeminarInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AuthCode from 'react-auth-code-input';
+import ReactCodesInput from 'react-codes-input';
 
 interface SeminarInputProps {
   disabled?: boolean;
@@ -13,27 +13,17 @@ const SeminarInput = ({ disabled, helperText, setInputCode, inputCode }: Seminar
 
   return (
     <div>
-      {disabled ? (
-        <AuthCode
-          disabled
-          placeholder="1234"
-          key={inputCode}
-          allowedCharacters="numeric"
-          onChange={setInputCode}
-          length={4}
-          containerClassName="flex h-[52px] w-[192px] justify-between"
-          inputClassName="w-[50px] border border-pointBlue bg-transparent text-3xl text-center mr-3 focus:outline-none"
-        />
-      ) : (
-        <AuthCode
-          allowedCharacters="numeric"
-          onChange={setInputCode}
-          length={4}
-          containerClassName="flex h-[52px] w-[192px] justify-between"
-          inputClassName="w-[50px] border border-pointBlue bg-transparent text-3xl text-center mr-3 focus:outline-none"
-          autoFocus
-        />
-      )}
+      <ReactCodesInput
+        disabled={disabled}
+        value={disabled ? inputCode : ''}
+        type="number"
+        onChange={setInputCode}
+        codeLength={4}
+        classNameCodeWrapperFocus="!border-0"
+        classNameWrapper="flex h-[52px] w-[192px] justify-between"
+        classNameCodeWrapper="w-[42px] border-b-2 border-pointBlue text-white bg-subBlack text-3xl text-center pt-2"
+      />
+
       <div className="my-[4px] flex items-center justify-center text-small text-red-500">{helperText}</div>
     </div>
   );
