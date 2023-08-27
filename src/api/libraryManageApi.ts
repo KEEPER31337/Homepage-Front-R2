88663ea95b3d4db027addd992b5ec9e3805e52de
pipisebastian <yrt7998@pusan.ac.kr>
2023-08-27 16:36:47 +0900

    feat : api 추가 및 연동
    #550

---
+++ src/api/libraryManageApi.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useQuery, useMutation } from 'react-query';
+import { BookManageInfo, BorrowInfo, BookListSearch } from './dto';
+
+const libraryManageKeys = {
+  bookManageList: (param: BookListSearch) => ['libraryManage', 'bookManageList', param] as const,
+};
+
+const useGetBookManageListQuery = (param: BookListSearch) => {
+  const fetcher = () =>
+    axios.get('/manage/books', { params: { ...param } }).then(({ data }) => {
+      const content = data.content.map((bookInfo: BookManageInfo) => ({
+        id: bookInfo.bookId,
+        title: bookInfo.title,
+        author: bookInfo.author,
+        bookQuantity: `${bookInfo.currentQuantity}/${bookInfo.totalQuantity}`,
+        borrowers: bookInfo.borrowInfos.map((borrowInfo) => borrowInfo.borrowerRealName).join(', '),
+        canBorrow: bookInfo.totalQuantity > bookInfo.currentQuantity,
+      }));
+      return { content, totalElement: data.totalElements };
+    });
+
+  return useQuery<{ content: BookManageInfo[]; totalElement: number }>(
+    libraryManageKeys.bookManageList(param),
+    fetcher,
+  );
+};
+
+const useDeleteBookMutation = () => {
+  const fetcher = (bookId: number) => axios.delete(`/manage/books/${bookId}`);
+
+  return useMutation(fetcher);
+};
+
+export { useGetBookManageListQuery, useDeleteBookMutation };
--- src/pages/admin/LibraryManage/Tab/BookManageTab.tsx
+++ src/pages/admin/LibraryManage/Tab/BookManageTab.tsx
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
 import { BookListInfo } from '@api/dto';
-
+import usePagination from '@hooks/usePagination';
+import { useGetBookManageListQuery } from '@api/libraryManageApi';
 import StandardTable from '@components/Table/StandardTable';
 import OutlinedButton from '@components/Button/OutlinedButton';
-import { ChildComponent } from '@components/Table/StandardTable.interface';
-import { columns, rows } from '@mocks/LibraryManageApi';
+import { Column, Row, ChildComponent } from '@components/Table/StandardTable.interface';
+
 import SearchSection from '@components/Section/SearchSection';
 import AddBookModal from '../Modal/AddBookModal';
 import DeleteBookModal from '../Modal/DeleteBookModal';
 
+interface libraryManageRow {
+  no: number;
+  title: string;
+  author: string;
+  bookQuantity: string;
+  borrowers: string;
+  canBorrow: boolean;
+}
+
+const libraryManageColumn: Column<libraryManageRow>[] = [
+  { key: 'no', headerName: '번호' },
+  { key: 'title', headerName: '도서명' },
+  { key: 'author', headerName: '저자' },
+  { key: 'bookQuantity', headerName: '대출현황' },
+  { key: 'borrowers', headerName: '대출자' },
+  {
+    key: 'canBorrow',
+    headerName: '대출상태',
+  },
+];
+
 const BookManageTab = () => {
-  const bookNumberList = [
+  const size = 10;
+  const { page, getRowNumber } = usePagination();
+
+  const { data: bookListData } = useGetBookManageListQuery({ page, size });
+
+  console.log(bookListData);
+  const selectorList = [
     { id: 'all', content: '도서명 + 저자' },
     { id: 'title', content: '도서명' },
     { id: 'author', content: '저자' },
@@ -22,9 +50,9 @@ const BookManageTab = () => {
     console.log(selectorValue, inputValue, '검색 api 호출');
   };
 
-  const childComponent = ({ key, value }: ChildComponent<BookListInfo>) => {
+  const childComponent = ({ key, value }: ChildComponent<libraryManageRow>) => {
     switch (key) {
-      case 'enable':
+      case 'canBorrow':
         return value ? <div className="text-pointBlue">대출가능</div> : <div className="text-subGray">대출불가</div>;
       default:
         return value;
@@ -33,11 +61,17 @@ const BookManageTab = () => {
   const [addBookModalOpen, setAddBookModalOpen] = useState(false);
   const [deleteBookModalOpen, setDeleteBookModalOpen] = useState(false);
 
+  const handlePostRowClick = ({ rowData }: { rowData: Row<libraryManageRow> }) => {
+    console.log(rowData.id, rowData);
+  };
+
+  if (!bookListData) return null;
+
   return (
     <>
       <div className="flex justify-between space-x-2">
         <SearchSection
-          options={bookNumberList}
+          options={selectorList}
           selectorValue={selectorValue}
           setSelectorValue={setSelectorValue}
           inputValue={inputValue}
@@ -49,7 +83,16 @@ const BookManageTab = () => {
       </div>
       {/* <OutlinedButton onClick={() => setDeleteBookModalOpen(true)}>도서 삭제</OutlinedButton>
       <DeleteBookModal open={deleteBookModalOpen} onClose={() => setDeleteBookModalOpen(false)} /> */}
-      <StandardTable<BookListInfo> columns={columns} rows={rows} childComponent={childComponent} />
+      <StandardTable
+        columns={libraryManageColumn}
+        rows={bookListData?.content.map((post, postIndex) => ({
+          no: getRowNumber({ size, index: postIndex }),
+          ...post,
+        }))}
+        childComponent={childComponent}
+        paginationOption={{ rowsPerPage: size, totalItems: bookListData?.totalElement }}
+        onRowClick={handlePostRowClick}
+      />
     </>
   );
 };
