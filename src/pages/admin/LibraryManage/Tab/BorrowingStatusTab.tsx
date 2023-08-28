fc07d2790108bae9a05b786f7ce2ab5c297f2a1f
pipisebastian <yrt7998@pusan.ac.kr>
2023-08-29 02:34:43 +0900

    refactor : 컴포넌트명 오타 수정
    #574

--- src/pages/admin/LibraryManage/Tab/BorrowingStatusTab.tsx
+++ src/pages/admin/LibraryManage/Tab/BorrowingStatusTab.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import OutlinedButton from '@components/Button/OutlinedButton';
 
-import OverDueBookModal from '../Modal/OverdueBookModal';
+import OverdueBookModal from '../Modal/OverdueBookModal';
 
 const BorrowingStatusTab = () => {
   const [addBookModalOpen, setAddBookModalOpen] = useState(false);
@@ -9,7 +9,7 @@ const BorrowingStatusTab = () => {
   return (
     <>
       <OutlinedButton onClick={() => setAddBookModalOpen(true)}>연체 도서</OutlinedButton>
-      {addBookModalOpen && <OverDueBookModal open={addBookModalOpen} onClose={() => setAddBookModalOpen(false)} />}
+      {addBookModalOpen && <OverdueBookModal open={addBookModalOpen} onClose={() => setAddBookModalOpen(false)} />}
     </>
   );
 };
