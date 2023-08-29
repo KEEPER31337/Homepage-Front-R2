c18d10ee3e5b34e5ab33cac75428b3348902738d
publdaze <publdaze@naver.com>
2023-08-29 19:36:20 +0900

    feat : 스터디 장, 현재 인원 배치
    #428

--- src/pages/Study/Accordion/StudyAccordionHeader.tsx
+++ src/pages/Study/Accordion/StudyAccordionHeader.tsx
@@ -2,9 +2,8 @@ import React, { Dispatch, DispatchWithoutAction, SetStateAction } from 'react';
 import ServerImg from '@components/Image/ServerImg';
 import { StudyInfo } from '@api/dto';
 import useCheckAuth from '@hooks/useCheckAuth';
-import { Typography } from '@mui/material';
+import { Chip, Divider, Typography } from '@mui/material';
 import ActionButton from '@components/Button/ActionButton';
-import OutlinedButton from '@components/Button/OutlinedButton';
 import { ModalInfo } from '../Study.interface';
 
 interface StudyAccordionHeaderProps {
@@ -25,36 +24,36 @@ const StudyAccordionHeader = ({ study, toggleOpen, setModalInfo }: StudyAccordio
   };
 
   return (
-    <div className="flex w-full items-center space-x-4 pl-2">
-      <ServerImg
-        className="h-16 w-16 object-cover"
-        errorClassName="h-16 w-16"
-        src={study.thumbnailPath}
-        alt="스터디 썸네일"
-      />
-      <Typography variant="h3" fontWeight="semiBold">
-        {study.title}
-      </Typography>
-      {checkIsMyId(study.headId) && (
-        <div className="space-x-2">
-          <ActionButton mode="edit" small onClick={handleStudyEditButtonClick}>
-            수정
-          </ActionButton>
-          <ActionButton mode="delete" small onClick={handleStudyDeleteButtonClick}>
-            삭제
-          </ActionButton>
-        </div>
-      )}
-      {/* <div className="flex w-full items-center justify-between space-x-2 pr-2">
-        <div className="flex items-center space-x-2">
-          <Typography className="!font-semibold">스터디장</Typography>
-          <StudyChip fontWeight="Semibold" value={study.headMember.realName} />
-          <Divider className="!border-white" orientation="vertical" flexItem />
-          <Typography>
-            현재 인원 <span className="font-semibold">{study.memberNumber}명</span>
-          </Typography>
-        </div>
-      </div> */}
+    <div className="flex w-full items-center justify-between px-2">
+      <div className="flex items-center space-x-4">
+        <ServerImg
+          className="h-16 w-16 object-cover"
+          errorClassName="h-16 w-16"
+          src={study.thumbnailPath}
+          alt="스터디 썸네일"
+        />
+        <Typography variant="h3" fontWeight="semiBold">
+          {study.title}
+        </Typography>
+        {checkIsMyId(study.headId) && (
+          <div className="space-x-2">
+            <ActionButton mode="edit" small onClick={handleStudyEditButtonClick}>
+              수정
+            </ActionButton>
+            <ActionButton mode="delete" small onClick={handleStudyDeleteButtonClick}>
+              삭제
+            </ActionButton>
+          </div>
+        )}
+      </div>
+      <div className="flex items-center space-x-2">
+        <Typography>스터디장</Typography>
+        <Chip className="!rounded-md !bg-pointBlue/30 font-semibold" label={study.headName} />
+        <Divider variant="middle" className="!border-white" orientation="vertical" flexItem />
+        <Typography>
+          현재 인원 <span className="font-semibold">{study.memberCount}명</span>
+        </Typography>
+      </div>
     </div>
   );
 };
