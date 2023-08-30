85d2f68c918098eac0ef1cf951851a06e90fff0f
publdaze <publdaze@naver.com>
2023-08-30 23:07:02 +0900

    feat : 아코디언 body 구성
    #428

--- src/pages/Study/Accordion/StudyAccordion.tsx
+++ src/pages/Study/Accordion/StudyAccordion.tsx
@@ -11,56 +11,13 @@ import { Link } from 'react-router-dom';
 import { ModalInfo } from '../Study.interface';
 import { StudyChip } from '../share/StudyChip';
 import StudyAccordionHeader from './StudyAccordionHeader';
+import StudyAccordionBody from './StudyAccordionBody';
 
 interface StudyAccordionProps {
   study: any;
   toggleOpen: DispatchWithoutAction;
   setModalInfo: Dispatch<SetStateAction<ModalInfo>>;
 }
-
-type StudyAccordionBodyProps = Pick<StudyAccordionProps, 'study'>;
-
-const StudyAccordionBody = ({ study }: StudyAccordionBodyProps) => {
-  interface IconType {
-    [key: string]: React.ReactNode;
-  }
-
-  const { information, memberList } = study;
-
-  const LinkIconData: IconType = {
-    github: <VscGithubInverted className="h-5 w-5" />,
-    notion: <SiNotion className="h-5 w-5" />,
-    etc: <VscLink className="h-5 w-5" />,
-  };
-
-  return (
-    <div className="space-y-[30px]">
-      <div className="flex justify-between space-y-4">
-        <Typography className="font-semibold">스터디 소개</Typography>
-        <Typography className="border-l-2 border-pointBlue px-2">{information}</Typography>
-      </div>
-      <div className="space-y-4">
-        <Typography className="font-semibold">스터디원</Typography>
-        <div className="flex space-x-2">
-          {memberList?.map(({ id, realName }: any) => (
-            <StudyChip key={id} value={realName} />
-          ))}
-        </div>
-      </div>
-      <div className="space-y-4">
-        <Typography className="font-semibold">링크</Typography>
-        <div className="flex space-x-3 text-pointBlue">
-          {/* {study.link?.map(({ title, contents }) => (
-            <Link to={contents} target="_blank" key={title} className="flex items-center space-x-1">
-              <span>{LinkIconData[title] ? LinkIconData[title] : LinkIconData.etc}</span>
-              <span className="border-b border-pointBlue">{title}</span>
-            </Link>
-          ))} */}
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const StudyAccordion = ({ study, toggleOpen, setModalInfo }: StudyAccordionProps) => {
   return (
@@ -72,7 +29,7 @@ const StudyAccordion = ({ study, toggleOpen, setModalInfo }: StudyAccordionProps
         <StudyAccordionHeader study={study} toggleOpen={toggleOpen} setModalInfo={setModalInfo} />
       </AccordionSummary>
       <AccordionDetails className="!space-y-[30px] !bg-middleBlack !px-[41px] !py-[30px] !text-white">
-        <StudyAccordionBody study={study} />
+        <StudyAccordionBody studyId={study.studyId} />
       </AccordionDetails>
     </Accordion>
   );
