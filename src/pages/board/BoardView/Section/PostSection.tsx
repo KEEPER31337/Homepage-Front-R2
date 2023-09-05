c76c9c7c115d8d0c4d6ed6be644bf65360fa7313
publdaze <publdaze@naver.com>
2023-09-05 18:12:52 +0900

    feat : 파일 조회 방식 변경
    #579

--- src/pages/board/BoardView/Section/PostSection.tsx
+++ src/pages/board/BoardView/Section/PostSection.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Button } from '@mui/material';
-import { VscArrowDown, VscArrowUp, VscFile } from 'react-icons/vsc';
+import { VscArrowDown, VscArrowUp, VscFolder, VscFolderOpened } from 'react-icons/vsc';
 import { PostInfo } from '@api/dto';
 import {
   useGetPostFilesQuery,
@@ -10,6 +10,7 @@ import {
 } from '@api/postApi';
 import FilledButton from '@components/Button/FilledButton';
 import OutlinedButton from '@components/Button/OutlinedButton';
+import FileViewer from '@components/Viewer/FileViewer';
 import StandardViewer from '@components/Viewer/StandardViewer';
 
 interface PostSectionProps {
@@ -18,10 +19,16 @@ interface PostSectionProps {
 }
 
 const PostSection = ({ postId, post }: PostSectionProps) => {
+  const [fileOpen, toggleFileOpen] = useReducer((prev) => !prev, false);
+
   const { data: files } = useGetPostFilesQuery(postId);
   const { mutate: controlLikes } = useControlPostLikesMutation();
   const { mutate: controlDislikes } = useControlPostDislikesMutation();
   const { mutate: downloadFile } = useDownloadFileMutation();
+
+  const handleFileOpenButtonClick = () => {
+    toggleFileOpen();
+  };
 
   const handleDownloadFileClick = (fileId: number, fileName: string) => {
     downloadFile({ postId, fileId, fileName });
@@ -37,17 +44,21 @@ const PostSection = ({ postId, post }: PostSectionProps) => {
 
   return (
     <div className="min-h-[520px] bg-middleBlack px-14 py-10">
-      <StandardViewer className="min-h-[330px]" content={post.content} />
-      <div className="mb-10 mt-2 flex justify-end gap-3 text-pointBlue">
-        {files &&
-          files.map((file) => (
-            <Button key={file.fileId} className="flex" onClick={() => handleDownloadFileClick(file.fileId, file.name)}>
-              <VscFile className="mr-1" size={24} />
-              <span>{file.name}</span>
-            </Button>
-          ))}
-      </div>
-      <div className="flex items-center justify-center space-x-2">
+      <StandardViewer className="mb-4 min-h-[330px]" content={post.content} />
+      <Button
+        className="hover:!bg-transparent"
+        variant="text"
+        onClick={handleFileOpenButtonClick}
+        startIcon={fileOpen ? <VscFolderOpened /> : <VscFolder />}
+      >
+        첨부파일
+      </Button>
+      {fileOpen && (
+        <div className="mb-10 mt-2 flex justify-end gap-3 text-pointBlue">
+          {files && <FileViewer files={files} onRowClick={handleDownloadFileClick} />}
+        </div>
+      )}
+      <div className="mt-8 flex items-center justify-center space-x-2">
         {post.isLike ? (
           <FilledButton small onClick={handleLikeButtonClick}>
             <VscArrowUp className="mr-1" size={10} />
