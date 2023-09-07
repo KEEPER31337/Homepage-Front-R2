fd9f2fd7ea50001559beace0a5dfe1c58cd09ef0
jasper200207 <jasper12394@gmail.com>
2023-09-07 12:03:38 +0900

    refector : queryKey 캐멀형식으로
    #567

--- src/api/meritApi.ts
+++ src/api/meritApi.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { PageAndSize, MeritLog, MeritType } from './dto';
 
 const meritKeys = {
-  meritLog: (param: PageAndSize) => ['merit-log', param] as const,
-  meritType: (param: PageAndSize) => ['merit-type', param] as const,
+  meritLog: (param: PageAndSize) => ['meritLog', param] as const,
+  meritType: (param: PageAndSize) => ['meritType', param] as const,
 };
 
 const useGetMeritLogQuery = ({ page, size = 10 }: PageAndSize) => {
