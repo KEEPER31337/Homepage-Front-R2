bcd6b0d9e8697427c27962d599ff762138d27ea6
publdaze <publdaze@naver.com>
2023-09-07 20:17:16 +0900

    feat : StandardDatePicker 배경색 확장 및 UI 개선
    #388, #296

--- src/components/DatePicker/StandardDatePicker.tsx
+++ src/components/DatePicker/StandardDatePicker.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { TextField } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
@@ -9,19 +8,31 @@ interface StandardDatePickerProps {
   date: DateTime | null;
   setDate: React.Dispatch<React.SetStateAction<DateTime | null>>;
   label?: string;
+  hasBackground?: boolean;
 }
 
-const StandardDatePicker = ({ date, setDate, label }: StandardDatePickerProps) => {
+const StandardDatePicker = ({ date, setDate, label, hasBackground }: StandardDatePickerProps) => {
   return (
     <LocalizationProvider dateAdapter={AdapterLuxon}>
       <DatePicker
         label={label}
         inputFormat="yyyy.MM.dd"
+        OpenPickerButtonProps={{ size: 'small', color: 'primary' }}
         value={date}
         onChange={(newValue) => {
           setDate(newValue);
         }}
-        renderInput={(params) => <TextField variant="standard" {...params} />}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            InputProps={{
+              className: `before:!border-pointBlue pr-1 ${hasBackground ? 'bg-subGray/5 h-12' : ''}`,
+              ...params.InputProps,
+            }}
+            sx={hasBackground ? { '.MuiFormLabel-root[data-shrink=false]': { top: 8 } } : undefined}
+          />
+        )}
       />
     </LocalizationProvider>
   );
