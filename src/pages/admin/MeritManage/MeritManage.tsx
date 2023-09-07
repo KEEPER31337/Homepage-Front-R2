8e2468a30a125d81a886c21e56f59cee435965c8
jasper200207 <jasper12394@gmail.com>
2023-09-07 18:10:33 +0900

    refector : tab 다시 변수로 꺼내기
    #567

--- src/pages/admin/MeritManage/MeritManage.tsx
+++ src/pages/admin/MeritManage/MeritManage.tsx
@@ -4,29 +4,27 @@ import MeritLogTab from './Tab/MeritLogTab';
 import MeritTypeTab from './Tab/MeritTypeTab';
 import UserMeritTab from './Tab/UserMeritTab';
 
+const meritTabs = [
+  {
+    id: 0,
+    label: '상벌점 내역',
+  },
+  {
+    id: 1,
+    label: '회원별 조회',
+  },
+  {
+    id: 2,
+    label: '사유 관리',
+  },
+];
+
 const MeritManage = () => {
   const [tab, setTab] = useState(0);
 
   return (
     <>
-      <StandardTab
-        options={[
-          {
-            id: 0,
-            label: '상벌점 내역',
-          },
-          {
-            id: 1,
-            label: '회원별 조회',
-          },
-          {
-            id: 2,
-            label: '사유 관리',
-          },
-        ]}
-        tab={tab}
-        setTab={setTab}
-      />
+      <StandardTab options={meritTabs} tab={tab} setTab={setTab} />
       {tab === 0 && <MeritLogTab />}
       {tab === 1 && <UserMeritTab />}
       {tab === 2 && <MeritTypeTab />}
