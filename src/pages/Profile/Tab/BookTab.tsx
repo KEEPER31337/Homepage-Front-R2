7f63275506ab12d24559521a4a352390a5988ac6
pipisebastian <yrt7998@pusan.ac.kr>
2023-09-08 22:09:01 +0900

    feat : 사서 이름 불러오기 api 연결
    #636

--- src/pages/Profile/Tab/BookTab.tsx
+++ src/pages/Profile/Tab/BookTab.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import { useGetExecutiveInfoQuery } from '@api/dutyManageApi';
 import { useGetBookBorrowsQuery } from '@api/libraryApi';
 import BookCard from './BookTab/BookCard';
 import BookGuide from './BookTab/BookGuide';
@@ -8,6 +9,9 @@ const MAX_BORROWABLE_BOOKS = 5;
 
 const BookTab = () => {
   const { data: borrowedBookListData } = useGetBookBorrowsQuery({ page: 0, size: MAX_BORROWABLE_BOOKS });
+  const { data: executiveInfos } = useGetExecutiveInfoQuery();
+
+  const librarian = executiveInfos?.find((role) => role.jobName === 'ROLE_사서')?.realName || '';
 
   return (
     <div className="flex w-full flex-col justify-center">
@@ -19,7 +23,7 @@ const BookTab = () => {
           대출기간은 승인된 대출일자로 부터 <span className="text-pointBlue">14</span>일간 입니다.
         </BookGuide>
         <BookGuide>
-          도서 관련 문의는 사서<span className="text-pointBlue">(김은지)</span>에게 문의 가능합니다.
+          도서 관련 문의는 사서<span className="text-pointBlue">({librarian})</span>에게 문의 가능합니다.
         </BookGuide>
       </div>
       <div className="flex h-full flex-col space-y-5">
