23dee98ead22abae4667e88d217d712193e5489e
pipisebastian <yrt7998@pusan.ac.kr>
2023-09-08 21:41:11 +0900

    design :  반응형 수정 #610

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -15,11 +15,11 @@ const Profile = () => {
   const [tab, setTab] = useState(0);
 
   return (
-    <div className="mb-40 mt-header grid justify-center xl:grid-cols-[25%_75%]">
-      <div className="flex w-full border border-subGray xl:w-[350px]">프로필</div>
-      <div className="flex w-[1080px] flex-col">
+    <div className="mb-40 mt-header flex w-full flex-col justify-start xl:flex-row xl:justify-center">
+      <div className="flex h-1/4 w-full border border-subGray xl:h-full xl:w-80">프로필</div>
+      <div className="flex h-3/4 w-full max-w-container flex-col xl:h-full">
         <StandardTab options={tabList} tab={tab} setTab={setTab} />
-        <div className="mt-4 h-full border border-subGray p-4">
+        <div className="mt-4 flex h-full border border-subGray p-4">
           {tab === 0 && <AttendanceTab />}
           {tab === 1 && <BoardTab />}
           {tab === 2 && <BookTab />}
