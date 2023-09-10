4b33608fca2d25e22339e19e3b8804b74b76755f
publdaze <publdaze@naver.com>
2023-09-10 19:08:27 +0900

    feat : 캘린더 pagination UI 구성
    #554

--- src/pages/Profile/Tab/AttendanceTab.tsx
+++ src/pages/Profile/Tab/AttendanceTab.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IconButton, Stack } from '@mui/material';
 import { ResponsiveCalendar } from '@nivo/calendar';
+import { VscChevronLeft, VscChevronRight } from 'react-icons/vsc';
 import { CallenderChartInfo } from '@api/dto';
 import { KEEPER_COLOR } from '@constants/keeperTheme';
 
@@ -7,24 +9,44 @@ const AttendanceTab = () => {
   const data: CallenderChartInfo[] = []; // TODO API 받아오기
   const year = { from: '2015-01-01', to: '2015-05-07' }; // TODO
 
+  const [, setCurrentIndex] = useState(0);
+
+  const handlePrevButtonClick = () => {
+    setCurrentIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const handleNextButtonClick = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
   return (
-    <div className="w-full">
-      <ResponsiveCalendar
-        theme={{
-          labels: { text: { fill: 'white' } },
-        }}
-        data={data}
-        from={year.from}
-        to={year.to}
-        align="top"
-        emptyColor={KEEPER_COLOR.subBlack}
-        colors={[KEEPER_COLOR.pointBlue]}
-        dayBorderColor={KEEPER_COLOR.middleBlack}
-        monthBorderColor={KEEPER_COLOR.middleBlack}
-        dayBorderWidth={4}
-        margin={{ top: 20, right: 70, bottom: 5, left: 70 }}
-      />
-    </div>
+    <Stack width="100%">
+      <div className="h-36">
+        <ResponsiveCalendar
+          theme={{
+            labels: { text: { fill: 'white' } },
+          }}
+          data={data}
+          from={year.from}
+          to={year.to}
+          align="top"
+          emptyColor={KEEPER_COLOR.subBlack}
+          colors={[KEEPER_COLOR.pointBlue]}
+          dayBorderColor={KEEPER_COLOR.middleBlack}
+          monthBorderColor={KEEPER_COLOR.middleBlack}
+          dayBorderWidth={4}
+          margin={{ top: 20, right: 70, bottom: 5, left: 70 }}
+        />
+      </div>
+      <div className="mr-16 flex items-center justify-end">
+        <IconButton onClick={handlePrevButtonClick} color="primary">
+          <VscChevronLeft />
+        </IconButton>
+        <IconButton onClick={handleNextButtonClick} color="primary">
+          <VscChevronRight />
+        </IconButton>
+      </div>
+    </Stack>
   );
 };
 
