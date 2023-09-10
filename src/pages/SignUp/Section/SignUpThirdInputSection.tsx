b8894ae1e5339dee641783ff6873537db3228f48
publdaze <publdaze@naver.com>
2023-09-10 10:37:34 +0900

    feat : 이메일 중복 체크 api 연결
    #297

--- src/pages/SignUp/Section/SignUpThirdInputSection.tsx
+++ src/pages/SignUp/Section/SignUpThirdInputSection.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Controller, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 import { DateTime } from 'luxon';
 import { useSetRecoilState } from 'recoil';
+import { useCheckEmailDuplicationQuery } from '@api/signUpApi';
 import { REQUIRE_ERROR_MSG } from '@constants/errorMsg';
 import { emailRegex } from '@utils/validateEmail';
 import OutlinedButton from '@components/Button/OutlinedButton';
@@ -21,8 +22,15 @@ const SignUpThirdInputSection = () => {
   const {
     control,
     handleSubmit,
+    getValues,
+    setError,
     formState: { isSubmitting, isValid },
   } = useForm({ mode: 'onBlur' });
+
+  const { data: isEmailDuplicate, isSuccess: checkEmailDuplicationSuccess } = useCheckEmailDuplicationQuery({
+    email: getValues('email'),
+    enabled: isEmailSent,
+  });
 
   const handleRequestVerificationCode = () => {
     setIsEmailSent(true);
@@ -34,6 +42,15 @@ const SignUpThirdInputSection = () => {
     // TODO 가입 성공 시
     navigate('/login');
   };
+
+  useEffect(() => {
+    if (!checkEmailDuplicationSuccess) return;
+
+    if (isEmailDuplicate.duplicate === true) {
+      setError('email', { message: '이미 존재하는 이메일입니다.' });
+      setIsEmailSent(false);
+    }
+  }, [checkEmailDuplicationSuccess]);
 
   return (
     <Stack component="form" spacing={2} onSubmit={handleSubmit(handleThirdStepFormSubmit)}>
@@ -48,14 +65,14 @@ const SignUpThirdInputSection = () => {
             message: '이메일 주소를 다시 확인해주세요.',
           },
         }}
-        render={({ field, fieldState: { error } }) => {
+        render={({ field, fieldState: { error, isDirty } }) => {
           return (
             <EmailAuthInput
               label="이메일"
               {...field}
               error={Boolean(error)}
               helperText={error?.message}
-              buttonDisabled={Boolean(error)}
+              buttonDisabled={Boolean(error) || !isDirty}
               onAuthButtonClick={handleRequestVerificationCode}
             />
           );
@@ -75,7 +92,7 @@ const SignUpThirdInputSection = () => {
               {...field}
               error={Boolean(error)}
               helperText={error?.message}
-              disabled={!isEmailSent}
+              disabled={!isEmailDuplicate || isEmailDuplicate.duplicate || !isEmailSent}
               expirationTime={DateTime.now().plus({ seconds: expiredSeconds })}
             />
           );
