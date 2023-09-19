c99ef71ff1dc3b2359e426a5e538bea10429b3d9
publdaze <publdaze@naver.com>
2023-09-19 17:44:02 +0900

    refactor : 계정 정보 수정 모달 구조 변경
    #679

--- src/pages/Profile/Modal/EditAccountModal.tsx
+++ src/pages/Profile/Modal/EditAccountModal.tsx
@@ -11,18 +11,13 @@ import EmailAuthInput from '@components/Input/EmailAuthInput';
 import TimerInput from '@components/Input/TimerInput';
 import ConfirmModal from '@components/Modal/ConfirmModal';
 
-interface EditAccountModalProps {
-  open: boolean;
-  onClose: () => void;
-}
-
-const EditAccountModal = ({ open, onClose }: EditAccountModalProps) => {
+const EditEmailSection = () => {
   const [expirationTime] = useState<DateTime | null>(null);
   const [isEmailSent, setIsEmailSent] = useState(false);
 
   const {
-    control: emailControl,
-    handleSubmit: handleEmailSubmit,
+    control,
+    handleSubmit,
     getValues,
     setError,
     formState: { isSubmitting, isValid },
@@ -37,7 +32,7 @@ const EditAccountModal = ({ open, onClose }: EditAccountModalProps) => {
     setIsEmailSent(true);
   };
 
-  const handleSecondStepFormSubmit: SubmitHandler<FieldValues> = () => {
+  const handleEmailFormSubmit: SubmitHandler<FieldValues> = () => {
     // TODO
   };
 
@@ -51,6 +46,83 @@ const EditAccountModal = ({ open, onClose }: EditAccountModalProps) => {
   }, [checkEmailDuplicationSuccess]);
 
   return (
+    <Stack
+      width={{ sm: '100%' }}
+      spacing={{ xs: 2, md: 4 }}
+      marginBottom={4}
+      justifyContent="space-between"
+      onSubmit={handleSubmit(handleEmailFormSubmit)}
+    >
+      <Stack spacing={1} component="form" onSubmit={handleSubmit(handleEmailFormSubmit)}>
+        <Typography fontWeight="semibold">이메일 변경</Typography>
+        <Controller
+          name="email"
+          defaultValue=""
+          control={control}
+          rules={{
+            required: REQUIRE_ERROR_MSG,
+            pattern: {
+              value: emailRegex,
+              message: '이메일 주소를 다시 확인해주세요.',
+            },
+          }}
+          render={({ field, fieldState: { error, isDirty } }) => {
+            return (
+              <EmailAuthInput
+                label="이메일"
+                {...field}
+                error={Boolean(error)}
+                helperText={error?.message}
+                inputDisabled={isEmailSent && checkEmailDuplicationSuccess}
+                buttonDisabled={Boolean(error) || !isDirty || isEmailSent}
+                onAuthButtonClick={handleRequestVerificationCode}
+              />
+            );
+          }}
+        />
+        <Controller
+          name="authCode"
+          defaultValue=""
+          control={control}
+          rules={{
+            required: REQUIRE_ERROR_MSG,
+          }}
+          render={({ field, fieldState: { error } }) => {
+            return (
+              <TimerInput
+                label="인증코드 입력"
+                {...field}
+                error={Boolean(error)}
+                helperText={error?.message}
+                disabled={
+                  !isEmailDuplicate ||
+                  isEmailDuplicate.duplicate ||
+                  !isEmailSent ||
+                  Boolean(expirationTime && expirationTime < DateTime.now())
+                }
+                expirationTime={expirationTime}
+              />
+            );
+          }}
+        />
+      </Stack>
+      <OutlinedButton
+        type="submit"
+        disabled={!isValid || isSubmitting || Boolean(expirationTime && expirationTime < DateTime.now())}
+      >
+        완료
+      </OutlinedButton>
+    </Stack>
+  );
+};
+
+interface EditAccountModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const EditAccountModal = ({ open, onClose }: EditAccountModalProps) => {
+  return (
     <ConfirmModal open={open} onClose={onClose} title="계정 정보 수정" modalWidth="md">
       <Stack
         divider={<Divider orientation="vertical" flexItem />}
@@ -59,77 +131,10 @@ const EditAccountModal = ({ open, onClose }: EditAccountModalProps) => {
         direction={{ sm: 'column', md: 'row' }}
         justifyContent="space-between"
       >
-        <Stack
-          width={{ sm: '100%' }}
-          spacing={{ xs: 2, md: 4 }}
-          marginBottom={4}
-          justifyContent="space-between"
-          onSubmit={handleEmailSubmit(handleSecondStepFormSubmit)}
-        >
-          <Stack spacing={1} component="form" onSubmit={handleEmailSubmit(handleSecondStepFormSubmit)}>
-            <Typography fontWeight="semibold">이메일 변경</Typography>
-            <Controller
-              name="email"
-              defaultValue=""
-              control={emailControl}
-              rules={{
-                required: REQUIRE_ERROR_MSG,
-                pattern: {
-                  value: emailRegex,
-                  message: '이메일 주소를 다시 확인해주세요.',
-                },
-              }}
-              render={({ field, fieldState: { error, isDirty } }) => {
-                return (
-                  <EmailAuthInput
-                    label="이메일"
-                    {...field}
-                    error={Boolean(error)}
-                    helperText={error?.message}
-                    inputDisabled={isEmailSent && checkEmailDuplicationSuccess}
-                    buttonDisabled={Boolean(error) || !isDirty || isEmailSent}
-                    onAuthButtonClick={handleRequestVerificationCode}
-                  />
-                );
-              }}
-            />
-            <Controller
-              name="authCode"
-              defaultValue=""
-              control={emailControl}
-              rules={{
-                required: REQUIRE_ERROR_MSG,
-              }}
-              render={({ field, fieldState: { error } }) => {
-                return (
-                  <TimerInput
-                    label="인증코드 입력"
-                    {...field}
-                    error={Boolean(error)}
-                    helperText={error?.message}
-                    disabled={
-                      !isEmailDuplicate ||
-                      isEmailDuplicate.duplicate ||
-                      !isEmailSent ||
-                      Boolean(expirationTime && expirationTime < DateTime.now())
-                    }
-                    expirationTime={expirationTime}
-                  />
-                );
-              }}
-            />
-          </Stack>
-          <OutlinedButton
-            type="submit"
-            disabled={!isValid || isSubmitting || Boolean(expirationTime && expirationTime < DateTime.now())}
-          >
-            완료
-          </OutlinedButton>
-        </Stack>
+        <EditEmailSection />
       </Stack>
     </ConfirmModal>
   );
 };
 
 export default EditAccountModal;
-
