582fc9c7e1fccd402268cf9613968fefb4846378
publdaze <publdaze@naver.com>
2023-09-22 16:37:46 +0900

    design : 헤더 반응형 및 UI 다듬기
    #700

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -4,6 +4,7 @@ import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import { VscAccount, VscGithubInverted, VscThreeBars } from 'react-icons/vsc';
 import { useRecoilValue } from 'recoil';
 import { ReactComponent as Logo } from '@assets/logo/logo_neon.svg';
+import { HEADER_HEIGHT } from '@constants/keeperTheme';
 import memberState from '@recoil/member.recoil';
 import FilledButton from '@components/Button/FilledButton';
 import AccountMenu from './Menu/AccountMenu';
@@ -28,8 +29,8 @@ const Header = () => {
   return (
     <AppBar
       position="fixed"
-      className="h-header border-b border-pointBlue !bg-mainBlack !bg-none"
-      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      className="h-14 border-b border-pointBlue !bg-mainBlack !bg-none sm:h-header"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, height: HEADER_HEIGHT }}
     >
       <Toolbar className="flex items-center justify-between">
         <div className="flex items-center">
@@ -55,7 +56,7 @@ const Header = () => {
               <IconButton target="_blank" href="https://github.com/KEEPER31337">
                 <VscGithubInverted fill="#4CEEF9" />
               </IconButton>
-              <IconButton onClick={handleAccountIconClick}>
+              <IconButton edge="end" onClick={handleAccountIconClick}>
                 <VscAccount fill="#4CEEF9" />
               </IconButton>
             </div>
