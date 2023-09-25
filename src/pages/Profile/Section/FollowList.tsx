078be5c4d6766c7ff77de39e7e128ee23cf07107
pipisebastian <yrt7998@pusan.ac.kr>
2023-09-25 18:24:53 +0900

    feat : 프로필 페이지 반응형 적용
    #740

--- src/pages/Profile/Section/FollowList.tsx
+++ src/pages/Profile/Section/FollowList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { List, ListItemIcon, ListItem, ListItemButton, ListItemText, Avatar } from '@mui/material';
+import { List, ListItemIcon, ListItem, ListItemButton, ListItemText, Avatar, Typography } from '@mui/material';
 import { FollowInfo } from '@api/dto';
 import { getServerImgUrl } from '@utils/converter';
 
@@ -12,7 +12,7 @@ const FollowList = ({ followlist }: FollowListProps) => {
   const navigate = useNavigate();
 
   return (
-    <List className="flex !h-[120px] flex-col !overflow-auto !bg-pointBlue/10 !p-0">
+    <List className="flex h-[60px] flex-col !overflow-auto !bg-pointBlue/10 !p-0 sm:h-[120px]">
       <ListItem className="flex flex-col" disablePadding>
         {followlist.map((followInfo) => (
           <ListItemButton
@@ -22,13 +22,15 @@ const FollowList = ({ followlist }: FollowListProps) => {
               navigate(`/profile/${followInfo.id}`);
             }}
           >
-            <ListItemIcon>
+            <ListItemIcon className="items-center">
               <Avatar
-                className="m-1 !h-8 !w-8 !bg-subBlack !text-white"
+                className="m-1 !h-5 !w-5 !bg-subBlack !text-white sm:!h-8 sm:!w-8"
                 src={followInfo?.thumbnailPath ? getServerImgUrl(followInfo?.thumbnailPath) : ''}
               />
+              <Typography className="!text-small sm:!text-paragraph">
+                {followInfo.generation}기 {followInfo.name}
+              </Typography>
             </ListItemIcon>
-            <ListItemText primary={`${followInfo.generation}기 ${followInfo.name}`} />
           </ListItemButton>
         ))}
       </ListItem>
