64429feb2ae4c116aee139a276df6e2f26833c68
pipisebastian <yrt7998@pusan.ac.kr>
2023-09-27 14:18:12 +0900

    feat : success부분 하나로 묶기
    #747

--- src/pages/admin/ActiveMemberManage/Modal/ChangeMemberTypeModal.tsx
+++ src/pages/admin/ActiveMemberManage/Modal/ChangeMemberTypeModal.tsx
@@ -27,19 +27,25 @@ const ChangeMemberTypeModal = ({
     return memberTypes.find((memberType) => memberType.typeId === typeId)?.renderType;
   };
 
+  const isQuitType = () => {
+    if (renderTypeName() === '탈퇴') return true;
+    return false;
+  };
+
+  const handleMutationSuccess = () => {
+    onClose();
+    setSelectedMemberList([]);
+  };
   const handleButtonClick = () => {
     const memberIds = selectedMemberList.map((item) => item.value as number);
 
-    if (renderTypeName() === '탈퇴') {
+    if (isQuitType()) {
       deleteMemberMutation(
         {
           memberIds,
         },
         {
-          onSuccess: () => {
-            onClose();
-            setSelectedMemberList([]);
-          },
+          onSuccess: handleMutationSuccess,
         },
       );
     } else {
@@ -49,10 +55,7 @@ const ChangeMemberTypeModal = ({
           typeId,
         },
         {
-          onSuccess: () => {
-            onClose();
-            setSelectedMemberList([]);
-          },
+          onSuccess: handleMutationSuccess,
         },
       );
     }
@@ -70,15 +73,9 @@ const ChangeMemberTypeModal = ({
     >
       <div className="space-y-5">
         <Typography>
-          총{' '}
-          <span className={`${renderTypeName() === '탈퇴' ? 'text-subRed' : 'text-pointBlue'}`}>
-            {selectedMemberList.length}
-          </span>
-          명을{' '}
-          <span className={`${renderTypeName() === '탈퇴' ? 'text-subRed' : 'text-pointBlue'}`}>
-            {renderTypeName()}
-          </span>{' '}
-          타입으로 변경하시겠습니까?
+          총 <span className={`${isQuitType() ? 'text-subRed' : 'text-pointBlue'}`}>{selectedMemberList.length}</span>
+          명을 <span className={`${isQuitType() ? 'text-subRed' : 'text-pointBlue'}`}>{renderTypeName()}</span> 타입으로
+          변경하시겠습니까?
         </Typography>
         <List className={`${selectedMemberList.length > 3 ? 'h-[185px]' : 'h-fit'} flex flex-col !overflow-auto !p-0`}>
           <ListItem className="flex flex-col" disablePadding>
