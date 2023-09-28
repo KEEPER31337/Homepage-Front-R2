ac1c8c1eff4db21fcb294a9b7f913f05b7ccaf05
publdaze <publdaze@naver.com>
2023-09-28 18:38:51 +0900

    feat : 비밀번호 찾기 다른 이메일로 전송 시에도 전송 상태 반영되도록 처리
    #629

--- src/pages/login/Search/SearchPWFirstStep.tsx
+++ src/pages/login/Search/SearchPWFirstStep.tsx
@@ -57,12 +57,12 @@ const SearchPWFirstStep = ({ setCurrentStep, form, setForm }: SearchPWFirstStepP
   };
 
   const handleRequestVerificationCode = () => {
+    setIsSent(true);
     if (form.id && form.email) {
       requestAuthcode(
         { loginId: form.id, email: form.email },
         {
           onSuccess: () => {
-            setIsSent(true);
             setMatchInfoModalOpen(false);
           },
           onError: () => {
@@ -116,8 +116,8 @@ const SearchPWFirstStep = ({ setCurrentStep, form, setForm }: SearchPWFirstStepP
           <EmailAuthInput
             className="w-full sm:w-[70%]"
             inputDisabled={isSent}
-            isLoading={isEmailSendLoading}
-            isSuccess={isEmailSendSuccess}
+            isLoading={isSent && isEmailSendLoading}
+            isSuccess={isSent && isEmailSendSuccess}
             value={form.email}
             onChange={handleChange}
             buttonDisabled={!isValidEmail || isSent}
@@ -139,7 +139,7 @@ const SearchPWFirstStep = ({ setCurrentStep, form, setForm }: SearchPWFirstStepP
             name="verificationCode"
             value={form.verificationCode}
             onChange={handleChange}
-            disabled={!isSent}
+            disabled={!(isSent && isEmailSendSuccess)}
             expirationTime={DateTime.now().plus({ seconds: TIMER_DURATION_SECOND })}
           />
         </div>
