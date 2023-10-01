665987e80209638c8bc2fd398470fd78a9cb2a3e
pipisebastian <yrt7998@pusan.ac.kr>
2023-10-01 20:32:50 +0900

    feat : overflow속성 추가
    #741

--- src/pages/Library/Card/BookCard.tsx
+++ src/pages/Library/Card/BookCard.tsx
@@ -14,12 +14,12 @@ const BookCard = ({ bookInfo, onRequestBook }: BookCardProps) => {
       <div className="mr-2 flex h-[120px] w-[85px] shrink-0 bg-middleBlack">
         <ServerImg src={bookInfo?.thumbnailPath} alt="library thumbnail" />
       </div>
-      <div className="relative grow truncate p-2">
+      <div className="relative grow overflow-auto p-2">
         <Typography className="!mb-2 truncate !font-semibold">{bookInfo.title}</Typography>
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 overflow-auto">
           <Typography className="truncate">저자 : {bookInfo.author}</Typography>
           <span className="text-pointBlue"> | </span>
-          <Typography>권수 : {bookInfo.bookQuantity}</Typography>
+          <Typography className="shrink-0">권수 : {bookInfo.bookQuantity}</Typography>
         </div>
 
         <div className="absolute bottom-0 right-0">
