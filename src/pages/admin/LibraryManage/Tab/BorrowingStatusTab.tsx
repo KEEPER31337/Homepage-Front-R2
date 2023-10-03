7b1d22f8a83d404de4206193142a76b9aedca6ca
pipisebastian <yrt7998@pusan.ac.kr>
2023-10-03 23:32:59 +0900

    feat : 테이블 width추가
    #772

--- src/pages/admin/LibraryManage/Tab/BorrowingStatusTab.tsx
+++ src/pages/admin/LibraryManage/Tab/BorrowingStatusTab.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { useMediaQuery, useTheme } from '@mui/material';
 import { BorrowLogListSearch } from '@api/dto';
 import { useGetBorrowLogListQuery } from '@api/libraryManageApi';
 import usePagination from '@hooks/usePagination';
@@ -22,21 +23,25 @@ interface BorrowLogRow {
 }
 
 const BorrowLogColumn: Column<BorrowLogRow>[] = [
-  { key: 'no', headerName: '번호' },
-  { key: 'bookTitle', headerName: '도서명' },
-  { key: 'author', headerName: '저자' },
-  { key: 'borrowerRealName', headerName: '대출자' },
-  { key: 'borrowDateTime', headerName: '대출 일자' },
-  { key: 'expireDateTime', headerName: '반납 예정일' },
-  { key: 'returnDateTime', headerName: '반납 일자' },
-  { key: 'rejectDateTime', headerName: '대출 반려 일자' },
-  { key: 'status', headerName: '유형' },
+  { key: 'no', headerName: '번호', width: '5%' },
+  { key: 'bookTitle', headerName: '도서명', width: '30%' },
+  { key: 'author', headerName: '저자', width: '25%' },
+  { key: 'borrowerRealName', headerName: '대출자', width: '10%' },
+  { key: 'borrowDateTime', headerName: '대출 일자', width: '5%' },
+  { key: 'expireDateTime', headerName: '반납 예정일', width: '5%' },
+  { key: 'returnDateTime', headerName: '반납 일자', width: '5%' },
+  { key: 'rejectDateTime', headerName: '대출 반려 일자', width: '5%' },
+  { key: 'status', headerName: '유형', width: '10%' },
 ];
 
 const BorrowingStatusTab = () => {
   const { page, getRowNumber } = usePagination();
   const [overdueBookModalOpen, setOverdueBookModalOpen] = useState(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const [searchParams] = useSearchParams();
+
   const searchType = searchParams.get('searchType') as BorrowLogListSearch['searchType'];
   const search = searchParams.get('search') as BorrowLogListSearch['search'];
 
@@ -44,9 +49,13 @@ const BorrowingStatusTab = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="mb-5 flex justify-between space-x-2">
+      <div className="mb-2 flex w-full flex-col items-start justify-between md:mb-5 md:flex-row md:items-center">
         <BorrowingStatusSearchSection />
-        <OutlinedButton onClick={() => setOverdueBookModalOpen(true)}>연체 도서</OutlinedButton>
+        <div className="items-ends mt-2 flex w-full justify-end space-x-4 md:mt-0">
+          <OutlinedButton small={isMobile} onClick={() => setOverdueBookModalOpen(true)}>
+            연체 도서
+          </OutlinedButton>
+        </div>
       </div>
       <StandardTable
         columns={BorrowLogColumn}
