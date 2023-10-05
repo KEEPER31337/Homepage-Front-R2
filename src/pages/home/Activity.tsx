0bb3cb09d85b47b9aa4000f1d33fdb3d896b1a72
publdaze <publdaze@naver.com>
2023-10-05 15:44:42 +0900

    feat : 메인페이지 Activity 반응형 적용
    #684

--- src/pages/home/Activity.tsx
+++ src/pages/home/Activity.tsx
@@ -11,9 +11,9 @@ const imagePath = [
   '/img/main/activity_docs.png',
 ];
 
-const SubTitleImageCard = ({ subtitleImage, index }: { subtitleImage: SubTitleImagesInfo; index: number }) => {
+const SubTitleImageCard = ({ subtitleImage }: { subtitleImage: SubTitleImagesInfo }) => {
   return (
-    <div className="group grid grid-cols-[45%_55%]">
+    <div className="group sm:grid sm:grid-cols-[45%_55%]">
       <div className="flex place-content-center place-items-center group-even:order-last">
         <img
           className={imagePath?.[subtitleImage?.id] ? 'w-full object-cover' : 'h-[960px] w-96 object-contain'}
@@ -21,7 +21,7 @@ const SubTitleImageCard = ({ subtitleImage, index }: { subtitleImage: SubTitleIm
           alt={subtitleImage?.subtitle}
         />
       </div>
-      <div className="flex flex-col place-content-center pl-14 text-left">
+      <div className="flex flex-col place-content-center px-14 py-10 text-left">
         <p className="mb-8 text-3xl font-bold text-pointBlue">{subtitleImage?.subtitle}</p>
         <p className="whitespace-pre-line bg-gradient-to-b from-gray-500 to-white bg-clip-text text-xl text-transparent">
           {subtitleImage?.staticWriteContents?.map((staticWriteInfo) => `${staticWriteInfo?.content}\n\n`)}
@@ -36,8 +36,8 @@ const Activity = () => {
 
   return (
     <div className="bg-mainBlack">
-      {activityList?.subtitleImages?.map((subtitleImage, index) => (
-        <SubTitleImageCard key={subtitleImage.id} index={index} subtitleImage={subtitleImage} />
+      {activityList?.subtitleImages?.map((subtitleImage) => (
+        <SubTitleImageCard key={subtitleImage.id} subtitleImage={subtitleImage} />
       ))}
     </div>
   );
