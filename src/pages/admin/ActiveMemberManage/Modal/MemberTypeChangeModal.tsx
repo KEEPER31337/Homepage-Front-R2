cfa6f94ef73d1cde5fd47ca0419366fcd320b3c4
pipisebastian <yrt7998@pusan.ac.kr>
2023-10-05 20:01:22 +0900

    refactor : list 필요없는 mui 컴포넌트 제거
    #47

---
+++ src/pages/admin/ActiveMemberManage/Modal/MemberTypeChangeModal.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { List, ListItemIcon, ListItem, ListItemText, Typography } from '@mui/material';
+import { useEditMemberTypeMutation, useDeleteMemberMutation } from '@api/memberApi';
+import { MultiAutoCompleteValue } from '@components/Input/AutoComplete';
+import ActionModal from '@components/Modal/ActionModal';
+import memberTypes from '../memberTypes';
+
+interface MemberTypeChangeModalProps {
+  open: boolean;
+  onClose: () => void;
+  typeId: number;
+  selectedMemberList: MultiAutoCompleteValue;
+  setSelectedMemberList: React.Dispatch<React.SetStateAction<MultiAutoCompleteValue>>;
+}
+
+const MemberTypeChangeModal = ({
+  open,
+  onClose,
+  typeId,
+  selectedMemberList,
+  setSelectedMemberList,
+}: MemberTypeChangeModalProps) => {
+  const { mutate: editMemberTypeMutation } = useEditMemberTypeMutation();
+  const { mutate: deleteMemberMutation } = useDeleteMemberMutation();
+
+  const renderTypeName = () => {
+    return memberTypes.find((memberType) => memberType.typeId === typeId)?.renderType;
+  };
+
+  const isQuitType = () => {
+    if (renderTypeName() === '탈퇴') return true;
+    return false;
+  };
+
+  const handleMutationSuccess = () => {
+    onClose();
+    setSelectedMemberList([]);
+  };
+  const handleButtonClick = () => {
+    const memberIds = selectedMemberList.map((item) => item.value as number);
+
+    if (isQuitType()) {
+      deleteMemberMutation(
+        {
+          memberIds,
+        },
+        {
+          onSuccess: handleMutationSuccess,
+        },
+      );
+    } else {
+      editMemberTypeMutation(
+        {
+          memberIds,
+          typeId,
+        },
+        {
+          onSuccess: handleMutationSuccess,
+        },
+      );
+    }
+  };
+
+  return (
+    <ActionModal
+      open={open}
+      onClose={onClose}
+      title="회원 타입 변경"
+      actionButtonName="변경"
+      onActionButonClick={() => {
+        handleButtonClick();
+      }}
+    >
+      <div className="space-y-5">
+        <Typography>
+          총 <span className={`${isQuitType() ? 'text-subRed' : 'text-pointBlue'}`}>{selectedMemberList.length}</span>
+          명을 <span className={`${isQuitType() ? 'text-subRed' : 'text-pointBlue'}`}>{renderTypeName()}</span> 타입으로
+          변경하시겠습니까?
+        </Typography>
+        <List className={`${selectedMemberList.length > 3 ? 'h-[185px]' : 'h-fit'} flex flex-col !overflow-auto !p-0`}>
+          {selectedMemberList.map((member) => (
+            <ListItem key={member.value as number}>
+              <ListItemText primary={member.label} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </ActionModal>
+  );
+};
+
+export default MemberTypeChangeModal;
