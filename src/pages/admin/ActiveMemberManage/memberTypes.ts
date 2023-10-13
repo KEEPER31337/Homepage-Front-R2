c409f34087a265f30e16a57a684953620f6a343f
pipisebastian <pipisebastian@pusan.ac.kr>
2023-10-13 22:57:19 +0900

    fix : 기수별 그룹화 및 비회원 색깔 추가
    #816

--- src/pages/admin/ActiveMemberManage/memberTypes.ts
+++ src/pages/admin/ActiveMemberManage/memberTypes.ts
@@ -9,7 +9,7 @@ const memberTypes: memberTypeInfo[] = [
   { type: '정회원', renderType: '활동회원', typeId: 2, color: 'pointBlue' },
   { type: '휴면회원', renderType: '휴면', typeId: 3, color: 'white' },
   { type: '졸업', renderType: '졸업', typeId: 4, color: 'mainBlack' },
-  { type: '비회원', renderType: '비회원', typeId: 1, color: '' },
+  { type: '비회원', renderType: '비회원', typeId: 1, color: 'pointBlue/20' },
   { type: '탈퇴', renderType: '탈퇴', typeId: 5, color: '' },
 ];
 
