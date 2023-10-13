50050a21d62eea87d41fea78113c224e530e57c3
publdaze <publdaze@naver.com>
2023-10-13 20:35:03 +0900

    fix : 게시글 제목 텍스트로 잘못 변경된 거 다시 변수로 받아오도록 처리

--- src/components/Card/PostingCard.tsx
+++ src/components/Card/PostingCard.tsx
@@ -13,11 +13,11 @@ export interface PostingCardProps extends CardMainInfoProps, CardDetailInfoProps
   thumbnailPath: string | null;
 }
 
-export const CardMainInfo = ({ registerTime }: CardMainInfoProps) => {
+export const CardMainInfo = ({ title, registerTime }: CardMainInfoProps) => {
   return (
     <div className="flex justify-between">
       <Typography className="w-11/12 truncate font-semibold" variant="paragraph">
-        title
+        {title}
       </Typography>
       {DateTime.fromISO(registerTime) >= DateTime.now().plus({ days: -1 }).startOf('day') && (
         <span className="m-auto h-4 w-4 rounded-sm bg-pointBlue text-center text-small leading-4 text-mainBlack">
