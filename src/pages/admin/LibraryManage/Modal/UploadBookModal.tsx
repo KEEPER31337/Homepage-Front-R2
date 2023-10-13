fc961fb1e5f3dfd274bb0556e4a722dae8feb2e0
pipisebastian <pipisebastian@pusan.ac.kr>
2023-10-13 21:56:49 +0900

    fix : react hook form 제거 #801

--- src/pages/admin/LibraryManage/Modal/UploadBookModal.tsx
+++ src/pages/admin/LibraryManage/Modal/UploadBookModal.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Controller, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import { Typography, Tooltip, Stack } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Typography, Tooltip } from '@mui/material';
 import { ManageBookInfo } from '@api/dto';
 import { useAddBookMutation, useEditBookInfoMutation, useEditBookThumbnailMutation } from '@api/libraryManageApi';
 import StandardInput from '@components/Input/StandardInput';
@@ -15,49 +14,105 @@ interface SelectorProps {
 }
 
 const UploadBookModal = ({ open, onClose, bookDetail }: SelectorProps) => {
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { isSubmitting, isValid },
-  } = useForm({ mode: 'onBlur' });
-
+  const [bookInfo, setBookInfo] = useState({
+    title: '',
+    author: '',
+  });
+  const { title, author } = bookInfo;
   const [totalQuantity, setTotalQuantity] = useState(1);
   const [thumbnail, setThumbnail] = useState<Blob | null>(null);
   const [isThumbnailChanged, setIsThumbnailChanged] = useState(false);
+
+  const [isInvalidTitle, setIsInvalidTitle] = useState(false);
+  const [isInvalidAuthor, setIsInvalidAuthor] = useState(false);
+
+  const [titleHelperText, setTitleHelperText] = useState('');
+  const [authorHelperText, setAuthorHelperText] = useState('');
 
   const { mutate: addBookMutation } = useAddBookMutation();
   const { mutate: editBookInfo } = useEditBookInfoMutation();
   const { mutate: editBookThumbnail } = useEditBookThumbnailMutation();
 
-  const handleAddBookButtonClick: SubmitHandler<FieldValues> = ({ title, author }) => {
-    addBookMutation(
-      { bookCoreData: { title, author, bookDepartment: 'ETC', totalQuantity }, thumbnail },
-      {
-        onSuccess: () => {
-          onClose();
-          reset();
-        },
-      },
-    );
+  const validate = () => {
+    const titleTrim = title.trim();
+    const authorTrim = author.trim();
+
+    if (titleTrim === '') {
+      setTitleHelperText('도서명을 입력해주세요');
+      setIsInvalidTitle(true);
+    }
+    if (authorTrim === '') {
+      setAuthorHelperText('저자명을 입력해주세요');
+      setIsInvalidAuthor(true);
+    }
+    if (titleTrim.length > 30) {
+      setTitleHelperText('도서명은 200자 이내여야 합니다.');
+      setIsInvalidTitle(true);
+    }
+    if (authorTrim.length > 20) {
+      setAuthorHelperText('저자명은 30자 이내여야 합니다.');
+      setIsInvalidAuthor(true);
+    }
+
+    return titleTrim !== '' && authorTrim !== '' && titleTrim.length <= 200 && authorTrim.length <= 30;
   };
 
-  const handleEditBookButtonClick: SubmitHandler<FieldValues> = ({ title, author }) => {
-    if (bookDetail?.bookId) {
+  const resetBookInfo = () => {
+    setBookInfo({
+      title: '',
+      author: '',
+    });
+    setTotalQuantity(1);
+  };
+
+  const handleBookInfoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setBookInfo({
+      ...bookInfo,
+      [name]: value,
+    });
+  };
+
+  const handleAddBookButtonClick = () => {
+    const isValid = validate();
+    if (isValid) {
+      addBookMutation(
+        { bookCoreData: { title, author, bookDepartment: 'ETC', totalQuantity }, thumbnail },
+        {
+          onSuccess: () => {
+            onClose();
+            resetBookInfo();
+          },
+        },
+      );
+    }
+  };
+  const handleEditBookButtonClick = () => {
+    const isValid = validate();
+    if (isValid && bookDetail?.bookId) {
       editBookInfo(
-        { bookCoreData: { title, author, bookDepartment: 'ETC', totalQuantity }, bookId: bookDetail.bookId },
+        { bookCoreData: { title, author, totalQuantity, bookDepartment: 'ETC' }, bookId: bookDetail.bookId },
         {
           onSuccess: () => {
             if (isThumbnailChanged) {
               editBookThumbnail({ bookId: bookDetail.bookId, thumbnail });
             }
             onClose();
-            reset();
           },
         },
       );
     }
   };
+
+  useEffect(() => {
+    if (bookDetail) {
+      setBookInfo({
+        title: bookDetail.title,
+        author: bookDetail.author,
+      });
+      setTotalQuantity(bookDetail.totalQuantity);
+    }
+  }, [bookDetail]);
 
   return (
     <ActionModal
@@ -65,65 +120,35 @@ const UploadBookModal = ({ open, onClose, bookDetail }: SelectorProps) => {
       onClose={onClose}
       title={`도서 ${bookDetail ? '수정' : '추가'}`}
       actionButtonName={bookDetail ? '수정' : '추가'}
-      onActionButonClick={bookDetail ? handleSubmit(handleEditBookButtonClick) : handleSubmit(handleAddBookButtonClick)}
-      actionButtonDisabled={isSubmitting || !isValid}
+      onActionButonClick={bookDetail ? handleEditBookButtonClick : handleAddBookButtonClick}
     >
       <div className="flex space-x-6">
-        <Stack spacing={2}>
-          <Controller
-            name="title"
-            defaultValue={bookDetail?.title || ''}
-            control={control}
-            rules={{
-              required: '도서명을 입력해주세요.',
-              maxLength: {
-                value: 200,
-                message: '200자 이하로 입력해주세요.',
-              },
-            }}
-            render={({ field, fieldState: { error } }) => {
-              return <StandardInput label="도서명" {...field} error={Boolean(error)} helperText={error?.message} />;
-            }}
-          />
-          <Controller
-            name="author"
-            defaultValue={bookDetail?.author || ''}
-            control={control}
-            rules={{
-              required: '저자를 입력해주세요.',
-              maxLength: {
-                value: 30,
-                message: '30자 이하로 입력해주세요.',
-              },
-            }}
-            render={({ field, fieldState: { error } }) => {
-              return <StandardInput label="저자" {...field} error={Boolean(error)} helperText={error?.message} />;
-            }}
-          />
-          <div className="relative">
-            <TotalBookNumberSelector value={totalQuantity} setValue={setTotalQuantity} />
-            <div className="absolute bottom-0 right-0">
-              {bookDetail && (
-                <Tooltip
-                  title={`대출 현황 ${bookDetail.currentQuantity}/${bookDetail.totalQuantity}`}
-                  componentsProps={{
-                    tooltip: {
-                      sx: {
-                        bgcolor: 'rgba(76, 238, 249, 0.15)',
-                        fontSize: '14px',
-                      },
-                    },
-                  }}
-                  placement="top"
-                >
-                  <Typography className="text-pointBlue">도서 현황</Typography>
-                </Tooltip>
-              )}
-            </div>
-          </div>
-        </Stack>
         <div className="space-y-5">
+          <div>
+            <Typography>도서명</Typography>
+            <StandardInput
+              error={isInvalidTitle}
+              helperText={titleHelperText}
+              name="title"
+              value={title}
+              onChange={handleBookInfoChange}
+            />
+          </div>
+          <div>
+            <Typography>저자</Typography>
+            <StandardInput
+              error={isInvalidAuthor}
+              helperText={authorHelperText}
+              name="author"
+              value={author}
+              onChange={handleBookInfoChange}
+            />
+          </div>
           <div className="relative">
+            <div>
+              <Typography>권수</Typography>
+              <TotalBookNumberSelector value={totalQuantity} setValue={setTotalQuantity} />
+            </div>
             <div className="absolute bottom-0 right-0">
               {bookDetail && (
                 <Tooltip
