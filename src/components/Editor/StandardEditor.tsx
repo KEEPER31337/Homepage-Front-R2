fc9433b6f09dc9daf2a77249ee215cdfe11aea49
publdaze <publdaze@naver.com>
2024-05-04 21:55:35 +0900

    feat : Toast UI에 이미지 삽입 시 서버에 저장한 이미지 띄워주도록 처리
    - 서버에 저장한 이미지 url 받아와 처리
    - #454

--- src/components/Editor/StandardEditor.tsx
+++ src/components/Editor/StandardEditor.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useMediaQuery, useTheme } from '@mui/material';
+import { HookMap } from '@toast-ui/editor';
 import { Editor, EditorProps } from '@toast-ui/react-editor';
 
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+import { useUploadPostImageMutation } from '@api/postApi';
+import { getServerImgUrl } from '@utils/converter';
 
 interface StandardEditorProps extends EditorProps {
   forwardedRef?: React.MutableRefObject<Editor>;
@@ -13,11 +16,29 @@ const StandardEditor = ({ forwardedRef, ...props }: StandardEditorProps) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const { mutate: uploadPostImageMutation } = useUploadPostImageMutation();
+
+  const handleImageUpload: HookMap['addImageBlobHook'] = (blob, callback) => {
+    // TODO: 이미지 크기가 30MB 넘어가면 에러 처리
+    // TODO: 서버에서 이미지 받아오는 동안 딜레이 처리
+    uploadPostImageMutation(
+      { file: blob },
+      {
+        onSuccess: ({ filePath }) => {
+          callback(getServerImgUrl(filePath));
+        },
+      },
+    );
+  };
+
   return (
     <Editor
       ref={forwardedRef}
       initialValue={props.initialValue ?? ''}
       placeholder="내용을 입력해주세요."
+      hooks={{
+        addImageBlobHook: handleImageUpload,
+      }}
       previewStyle={isMobile ? 'tab' : 'vertical'}
       minHeight="300px"
       initialEditType={isMobile ? 'wysiwyg' : 'markdown'}
